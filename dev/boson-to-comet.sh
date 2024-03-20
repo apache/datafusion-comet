fa0c2f452c0141bf34f5fd12ee8b0a7ec80b2508
Steve Vaughan Jr <s_vaughan@apple.com>
2024-03-20 14:11:17 -0400

    fix: Check for `*.sql`, `*.js` and `*.css` for boson -> comet

--- dev/boson-to-comet.sh
+++ dev/boson-to-comet.sh
@@ -6,7 +6,7 @@ sed -i.bak -E 's/boson/comet/g' $SPARK_BASE/pom.xml
 sed -i.bak -E 's/com.apple.pie.boson/org.apache.comet/g' $SPARK_BASE/sql/core/pom.xml
 sed -i.bak -E 's/boson/comet/g' $SPARK_BASE/sql/core/pom.xml
 
-BOSON_FILES=$(find $SPARK_BASE \( -name "*.java" -o -name "*.scala" -o -name "*.xml" \) | xargs grep -li boson)
+BOSON_FILES=$(find $SPARK_BASE \( -name "*.java" -o -name "*.scala" -o -name "*.xml" -o -name "*.sql" -o -name "*.js" -o -name "*.css" \) | xargs grep -li boson)
 for file in $BOSON_FILES; do
   sed -i.bak -E 's/com.apple.boson/org.apache.comet/g' $file
   sed -i.bak -E 's/org.apache.spark.sql.boson/org.apache.spark.sql.comet/g' $file
