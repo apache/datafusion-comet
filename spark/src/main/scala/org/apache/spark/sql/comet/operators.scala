187ba365e58a5d9da103e1e36f1569723347c6d7
Liang-Chi Hsieh <viirya@gmail.com>
2024-03-21 13:49:31 -0700

    Remove duplicate code

--- spark/src/main/scala/org/apache/spark/sql/comet/operators.scala
+++ spark/src/main/scala/org/apache/spark/sql/comet/operators.scala
@@ -589,43 +589,6 @@ case class CometHashAggregateExec(
     Objects.hashCode(groupingExpressions, aggregateExpressions, input, mode, child)
 }
 
-case class CometHashJoinExec(
-    override val nativeOp: Operator,
-    override val originalPlan: SparkPlan,
-    leftKeys: Seq[Expression],
-    rightKeys: Seq[Expression],
-    joinType: JoinType,
-    condition: Option[Expression],
-    buildSide: BuildSide,
-    override val left: SparkPlan,
-    override val right: SparkPlan,
-    override val serializedPlanOpt: SerializedPlan)
-    extends CometBinaryExec {
-  override def withNewChildrenInternal(newLeft: SparkPlan, newRight: SparkPlan): SparkPlan =
-    this.copy(left = newLeft, right = newRight)
-
-  override def stringArgs: Iterator[Any] =
-    Iterator(leftKeys, rightKeys, joinType, condition, left, right)
-
-  override def equals(obj: Any): Boolean = {
-    obj match {
-      case other: CometHashJoinExec =>
-        this.leftKeys == other.leftKeys &&
-        this.rightKeys == other.rightKeys &&
-        this.condition == other.condition &&
-        this.buildSide == other.buildSide &&
-        this.left == other.left &&
-        this.right == other.right &&
-        this.serializedPlanOpt == other.serializedPlanOpt
-      case _ =>
-        false
-    }
-  }
-
-  override def hashCode(): Int =
-    Objects.hashCode(leftKeys, rightKeys, condition, left, right)
-}
-
 case class CometSortMergeJoinExec(
     override val nativeOp: Operator,
     override val originalPlan: SparkPlan,
