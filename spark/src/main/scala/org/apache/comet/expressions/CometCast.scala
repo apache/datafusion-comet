41b727313c5cda4f681e277947e6282ff17553e6
Rohit Rastogi <rohitrastogi@Rohits-MacBook-Pro.local>
2024-05-06 00:16:25 -0700

    make format

--- spark/src/main/scala/org/apache/comet/expressions/CometCast.scala
+++ spark/src/main/scala/org/apache/comet/expressions/CometCast.scala
@@ -178,12 +178,16 @@ object CometCast {
   }
 
   private def canCastFromFloat(toType: DataType): SupportLevel = toType match {
-    case DataTypes.BooleanType | DataTypes.DoubleType | DataTypes.IntegerType | DataTypes.LongType | DataTypes.ShortType | DataTypes.ByteType => Compatible
+    case DataTypes.BooleanType | DataTypes.DoubleType | DataTypes.IntegerType |
+        DataTypes.LongType | DataTypes.ShortType | DataTypes.ByteType =>
+      Compatible
     case _ => Unsupported
   }
 
   private def canCastFromDouble(toType: DataType): SupportLevel = toType match {
-    case DataTypes.BooleanType | DataTypes.FloatType | DataTypes.IntegerType | DataTypes.LongType | DataTypes.ShortType | DataTypes.ByteType => Compatible
+    case DataTypes.BooleanType | DataTypes.FloatType | DataTypes.IntegerType |
+        DataTypes.LongType | DataTypes.ShortType | DataTypes.ByteType =>
+      Compatible
     case _: DecimalType => Incompatible()
     case _ => Unsupported
   }
