c53219958a405fa379a95257f949fa96eaf815e7
Vipul Vaibhaw <vaibhaw.vipul@gmail.com>
2024-05-07 21:43:21 +0530

    Remove redundant comment

--- spark/src/test/scala/org/apache/comet/CometCastSuite.scala
+++ spark/src/test/scala/org/apache/comet/CometCastSuite.scala
@@ -403,7 +403,6 @@ class CometCastSuite extends CometTestBase with AdaptiveSparkPlanHelper {
   }
 
   test("cast DoubleType to DecimalType(10,2)") {
-    // Comet should have failed with [NUMERIC_VALUE_OUT_OF_RANGE]
     castTest(generateDoubles(), DataTypes.createDecimalType(10, 2))
   }
 
