501d2ecda31845c8b4fb7f772b1c2d3e0016da7a
comphead <comphead@ukr.net>
2024-05-10 10:14:23 -0700

    Coverage: Add a manual test for DF supporting Spark expressions directly

--- spark/src/test/scala/org/apache/comet/CometExpressionCoverageSuite.scala
+++ spark/src/test/scala/org/apache/comet/CometExpressionCoverageSuite.scala
@@ -25,27 +25,28 @@ import java.nio.file.{Files, Paths}
 import scala.collection.mutable
 import scala.sys.process._
 
-import org.scalatest.Ignore
 import org.scalatest.exceptions.TestFailedException
 
 import org.apache.hadoop.fs.Path
 import org.apache.spark.sql.CometTestBase
 import org.apache.spark.sql.execution.adaptive.AdaptiveSparkPlanHelper
 
+import org.apache.comet.CoverageResultStatus.CoverageResultStatus
+
 /**
  * Manual test to calculate Spark builtin expressions coverage support by the Comet
  *
- * The test will update files doc/spark_builtin_expr_coverage.txt,
- * doc/spark_builtin_expr_coverage_agg.txt
+ * The test will update files docs/spark_builtin_expr_coverage.txt,
+ * docs/spark_builtin_expr_coverage_agg.txt
  */
-@Ignore
+//@Ignore
 class CometExpressionCoverageSuite extends CometTestBase with AdaptiveSparkPlanHelper {
 
   import testImplicits._
 
-  private val rawCoverageFilePath = "doc/spark_builtin_expr_coverage.txt"
-  private val aggCoverageFilePath = "doc/spark_builtin_expr_coverage_agg.txt"
-  private val rawCoverageFileDatafusionPath = "doc/spark_builtin_expr_df_coverage.txt"
+  private val projectDocFolder = "docs"
+  private val rawCoverageFilePath = s"$projectDocFolder/spark_builtin_expr_coverage.txt"
+  private val DATAFUSIONCLI_PATH_ENV_VAR = "DATAFUSIONCLI_PATH"
 
   private val queryPattern = """(?i)SELECT (.+?);""".r
   private val valuesPattern = """(?i)FROM VALUES(.+?);""".r
@@ -72,6 +73,7 @@ class CometExpressionCoverageSuite extends CometTestBase with AdaptiveSparkPlanH
    * doc/spark_builtin_expr_coverage_agg.txt
    */
   test("Test Spark builtin expressions coverage") {
+    testNames
     val builtinExamplesMap = getExamples()
 
     // key - function name
@@ -80,6 +82,7 @@ class CometExpressionCoverageSuite extends CometTestBase with AdaptiveSparkPlanH
 
     builtinExamplesMap.foreach {
       case (funcName, q :: _) =>
+        var dfMessage: Option[String] = None
         val queryResult =
           try {
             // Example with predefined values
@@ -91,8 +94,13 @@ class CometExpressionCoverageSuite extends CometTestBase with AdaptiveSparkPlanH
               val values = valuesPattern.findFirstMatchIn(q).map(_.group(0))
               (select, values) match {
                 case (Some(s), Some(v)) =>
-                  testSingleLineQuery(s"select * $v", s"$s tbl")
+                  withTempDir { dir =>
+                    val path = new Path(dir.toURI.toString).toUri.getPath
+                    spark.sql(s"select * $v").repartition(1).write.mode("overwrite").parquet(path)
+                    dfMessage = runDatafusionCli(s"""$s '$path/*.parquet'""")
+                  }
 
+                  testSingleLineQuery(s"select * $v", s"$s tbl")
                 case _ =>
                   sys.error("Cannot parse properly")
               }
@@ -104,144 +112,113 @@ class CometExpressionCoverageSuite extends CometTestBase with AdaptiveSparkPlanH
               //
               // ConstantFolding is a operator optimization rule in Catalyst that replaces expressions
               // that can be statically evaluated with their equivalent literal values.
+              dfMessage = runDatafusionCli(q)
               testSingleLineQuery(
                 "select 'dummy' x",
                 s"${q.dropRight(1)}, x from tbl",
                 excludedOptimizerRules =
                   Some("org.apache.spark.sql.catalyst.optimizer.ConstantFolding"))
             }
-            CoverageResult(CoverageResultStatus.Passed.toString, Seq((q, "OK")))
+            CoverageResult(
+              q,
+              CoverageResultStatus.Passed,
+              CoverageResultDetails(
+                cometMessage = "OK",
+                datafusionMessage = dfMessage.getOrElse("OK")))
+
           } catch {
             case e: TestFailedException
-                if e.message.getOrElse("").contains("Expected only Comet native operators") =>
-              CoverageResult(CoverageResultStatus.Failed.toString, Seq((q, "Unsupported")))
+                if e.getMessage.contains("Expected only Comet native operators") =>
+              CoverageResult(
+                q,
+                CoverageResultStatus.Failed,
+                CoverageResultDetails(
+                  cometMessage =
+                    "Unsupported: Expected only Comet native operators but found Spark fallback",
+                  datafusionMessage = dfMessage.getOrElse("")))
+
             case e if e.getMessage.contains("CometNativeException") =>
               CoverageResult(
-                CoverageResultStatus.Failed.toString,
-                Seq((q, "Failed on native side")))
-            case _ =>
+                q,
+                CoverageResultStatus.Failed,
+                CoverageResultDetails(
+                  cometMessage = "Failed on native side: found CometNativeException",
+                  datafusionMessage = dfMessage.getOrElse("")))
+
+            case e =>
               CoverageResult(
-                CoverageResultStatus.Failed.toString,
-                Seq((q, "Failed on something else. Check query manually")))
+                q,
+                CoverageResultStatus.Failed,
+                CoverageResultDetails(
+                  cometMessage = e.getMessage,
+                  datafusionMessage = dfMessage.getOrElse("")))
           }
         resultsMap.put(funcName, queryResult)
 
+      // Function with no examples
       case (funcName, List()) =>
         resultsMap.put(
           funcName,
           CoverageResult(
-            CoverageResultStatus.Skipped.toString,
-            Seq(("", "No examples found in spark.sessionState.functionRegistry"))))
+            "",
+            CoverageResultStatus.Skipped,
+            CoverageResultDetails(
+              cometMessage = "No examples found in spark.sessionState.functionRegistry",
+              datafusionMessage = "")))
     }
 
     // TODO: convert results into HTML or .md file
     resultsMap.toSeq.toDF("name", "details").createOrReplaceTempView("t")
-    val str_agg = showString(
+//    val str_agg = showString(
+//      spark.sql(
+//        "select result, d._2 as details, count(1) cnt from (select name, t.details.result, explode_outer(t.details.details) as d from t) group by 1, 2 order by 1"),
+//      1000,
+//      0)
+//    Files.write(Paths.get(aggCoverageFilePath), str_agg.getBytes(StandardCharsets.UTF_8))
+
+    val str = showString(
       spark.sql(
-        "select result, d._2 as details, count(1) cnt from (select name, t.details.result, explode_outer(t.details.details) as d from t) group by 1, 2 order by 1"),
+        "select name, details.query, details.result, details.details.cometMessage, details.details.datafusionMessage from t order by 1"),
       1000,
       0)
-    Files.write(Paths.get(aggCoverageFilePath), str_agg.getBytes(StandardCharsets.UTF_8))
-
-    val str = showString(spark.sql("select * from t order by 1"), 1000, 0)
     Files.write(Paths.get(rawCoverageFilePath), str.getBytes(StandardCharsets.UTF_8))
   }
 
-  /**
-   * Manual test to calculate Spark builtin expressions coverage support by the Datafusion
-   * directly
-   *
-   * The test will update files doc/spark_builtin_expr_df_coverage.txt,
-   *
-   * Requires to set DATAFUSIONCLI_PATH env variable to valid path to datafusion-cli
-   */
-  test("Test Spark builtin expressions coverage by Datafusion directly") {
-    val builtinExamplesMap = getExamples()
-
-    // key - function name
-    // value - list of result shows if function supported by Datafusion
-    val resultsMap = new mutable.HashMap[String, CoverageResult]()
-
-    builtinExamplesMap.foreach {
-      case (funcName, q :: _) =>
-        val queryResult =
-          try {
-            // Example with predefined values
-            // e.g. SELECT bit_xor(col) FROM VALUES (3), (5) AS tab(col)
-            if (q.toLowerCase.contains(" from values")) {
-              val select = selectPattern.findFirstMatchIn(q).map(_.group(0))
-              val values = valuesPattern.findFirstMatchIn(q).map(_.group(0))
-              (select, values) match {
-                case (Some(s), Some(v)) =>
-                  withTempDir { dir =>
-                    val path = new Path(dir.toURI.toString).toUri.getPath
-                    spark.sql(s"select * $v").repartition(1).write.mode("overwrite").parquet(path)
-                    runDatafusionCli(s"""$s '$path/*.parquet'""")
-                  }
-
-                case _ =>
-                  sys.error("Cannot parse properly")
-              }
-            } else {
-              // Process the simple example like `SELECT cos(0);`
-              runDatafusionCli(q)
-            }
-            CoverageResult(CoverageResultStatus.Passed.toString, Seq((q, "OK")))
-          } catch {
-            case e: Throwable =>
-              CoverageResult(CoverageResultStatus.Failed.toString, Seq((q, e.getMessage)))
-          }
-        resultsMap.put(funcName, queryResult)
-
-      case (funcName, List()) =>
-        resultsMap.put(
-          funcName,
-          CoverageResult(
-            CoverageResultStatus.Skipped.toString,
-            Seq(("", "No examples found in spark.sessionState.functionRegistry"))))
-    }
-
-    resultsMap.toSeq.toDF("name", "details").createOrReplaceTempView("t")
-    val str = showString(spark.sql("select * from t order by 1"), 1000, 0)
-    Files.write(Paths.get(rawCoverageFileDatafusionPath), str.getBytes(StandardCharsets.UTF_8))
-  }
-
-  private def runDatafusionCli(sql: String) = {
+  // Returns error, None means successful execution
+  private def runDatafusionCli(sql: String): Option[String] = {
 
     val datafusionCliPath = sys.env.getOrElse(
-      "DATAFUSIONCLI_PATH",
-      sys.error("DATAFUSIONCLI_PATH env variable not set"))
+      DATAFUSIONCLI_PATH_ENV_VAR,
+      return Some(s"$DATAFUSIONCLI_PATH_ENV_VAR env variable not set"))
 
     val tempFilePath = Files.createTempFile("temp-", ".sql")
-    try {
-    Files.write(tempFilePath, sql.getBytes)
-   
-    val command = s"""$datafusionCliPath/datafusion-cli -f $tempFilePath"""
-
     val stdout = new StringBuilder
     val stderr = new StringBuilder
+    try {
+      Files.write(tempFilePath, sql.getBytes)
 
-    command.!(
-      ProcessLogger(
-        out => stdout.append(out).append("\n"), // stdout
-        err => stderr.append(err).append("\n") // stderr
-      ))
-   } finally {
+      val command = s"""$datafusionCliPath/datafusion-cli -f $tempFilePath"""
+      command.!(
+        ProcessLogger(
+          out => stdout.append(out).append("\n"), // stdout
+          err => stderr.append(err).append("\n") // stderr
+        ))
+    } finally {
       Files.delete(tempFilePath)
-   }
+    }
 
-    val err = stderr.toString()
-    val out = stdout.toString()
+    val err = stderr.toString
+    val out = stdout.toString
 
     if (err.nonEmpty)
-      sys.error(s"std_err: $err")
+      return Some(s"std_err: $err")
 
     if (out.toLowerCase.contains("error"))
-      sys.error(s"std_out: $out")
+      return Some(s"std_out: $out")
+
+    None
   }
 }
-
-case class CoverageResult(result: String, details: Seq[(String, String)])
 
 object CoverageResultStatus extends Enumeration {
   type CoverageResultStatus = Value
@@ -250,3 +227,10 @@ object CoverageResultStatus extends Enumeration {
   val Passed: Value = Value("PASSED")
   val Skipped: Value = Value("SKIPPED")
 }
+
+case class CoverageResult(
+    query: String,
+    result: CoverageResultStatus,
+    details: CoverageResultDetails)
+
+case class CoverageResultDetails(cometMessage: String, datafusionMessage: String)
