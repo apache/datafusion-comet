52aa474565f7553fa7e9a1450fd178cd4a21f953
Andy Grove <agrove@apache.org>
2024-06-10 13:14:34 -0600

    smaller change

--- core/src/parquet/read/values.rs
+++ core/src/parquet/read/values.rs
@@ -15,7 +15,7 @@
 // specific language governing permissions and limitations
 // under the License.
 
-use std::{marker::PhantomData, mem};
+use std::{marker::PhantomData, mem, ptr::copy_nonoverlapping};
 
 use arrow::buffer::Buffer;
 use bytes::Buf;
@@ -443,19 +443,49 @@ macro_rules! make_int_variant_impl {
     ($ty: ident, $native_ty: ty, $type_size: expr) => {
         impl PlainDecoding for $ty {
             fn decode(src: &mut PlainDecoderInner, dst: &mut ParquetMutableVector, num: usize) {
-                assert!(src.offset % 4 == 0);
-                let src_ptr = src.data.as_ptr() as *const i32;
-                let dst_ptr = dst.value_buffer.as_mut_ptr() as *mut $native_ty;
-                let offset_i32 = src.offset / 4;
-                unsafe {
-                    for i in 0..num {
-                        let ptr = src_ptr.add(offset_i32 + i) as *const i32;
-                        dst_ptr
-                            .add(dst.num_values + i)
-                            .write_unaligned(ptr.read_unaligned() as $native_ty);
+                let num_bytes = 4 * num; // Parquet stores Int8/Int16 using 4 bytes
+
+                let src_data = &src.data;
+                let mut src_offset = src.offset;
+                let dst_slice = dst.value_buffer.as_slice_mut();
+                let mut dst_offset = dst.num_values * $type_size;
+
+                let mut i = 0;
+                let mut in_ptr = &src_data[src_offset..] as *const [u8] as *const u8 as *const u32;
+
+                while num - i >= 32 {
+                    unsafe {
+                        //let in_slice = std::slice::from_raw_parts(in_ptr, 32);
+                        for _ in 0..32 {
+                            copy_nonoverlapping(
+                                //in_slice[n..].as_ptr() as *const $native_ty,
+                                &src_data[src_offset..] as *const [u8] as *const u8
+                                    as *const $native_ty,
+                                &mut dst_slice[dst_offset] as *mut u8 as *mut $native_ty,
+                                1,
+                            );
+                            i += 1;
+                            src_offset += 4;
+                            dst_offset += $type_size;
+                        }
+                        in_ptr = in_ptr.offset(32);
                     }
                 }
-                src.offset += 4 * num;
+
+                (0..(num - i)).for_each(|_| {
+                    unsafe {
+                        copy_nonoverlapping(
+                            &src_data[src_offset..] as *const [u8] as *const u8
+                                as *const $native_ty,
+                            &mut dst_slice[dst_offset] as *mut u8 as *mut $native_ty,
+                            1,
+                        );
+                    }
+                    src_offset += 4;
+                    dst_offset += $type_size;
+                });
+
+                src.offset += num_bytes;
             }
 
             fn skip(src: &mut PlainDecoderInner, num: usize) {
