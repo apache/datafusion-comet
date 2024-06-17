1002268eb38c0f623ccef19b826dc367687209fe
Liang-Chi Hsieh <viirya@gmail.com>
2024-06-17 10:33:23 -0700

    fix

--- spark/src/main/scala/org/apache/spark/sql/comet/operators.scala
+++ spark/src/main/scala/org/apache/spark/sql/comet/operators.scala
@@ -383,8 +383,8 @@ abstract class CometNativeExec extends CometExec {
     def transform(arg: Any): AnyRef = arg match {
       case sparkPlan: SparkPlan if !sparkPlan.isInstanceOf[CometNativeExec] =>
         // Different to Spark, Comet native query node might have a Spark plan as Product element.
-        // We need to canonicalize the Spark plan. But it is not possibly to be another Comet
-        // native query node.
+        // We need to canonicalize the Spark plan. But it cannot be another Comet native query node,
+        // otherwise it will cause recursive canonicalization.
         sparkPlan.canonicalized
       case other: AnyRef => other
       case null => null
