0213ecab7f56eba370bc27d1cd119f04a52f3835
Andy Grove <agrove@apache.org>
2024-07-05 10:50:08 -0600

    revert change

--- core/src/errors.rs
+++ core/src/errors.rs
@@ -585,7 +585,6 @@ mod tests {
         jvm().attach_current_thread().expect("Unable to attach JVM")
     }
 
-    #[ignore]
     #[test]
     pub fn error_from_panic() {
         let _guard = attach_current_thread();
