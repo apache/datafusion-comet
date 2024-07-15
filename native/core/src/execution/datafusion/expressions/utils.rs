dcb75e3302e3e400c319530bce08aae4f7a8a78f
Andy Grove <agrove@apache.org>
2024-07-15 11:47:44 -0600

    chore: Move temporal kernels and expressions to spark-expr crate (#660)
    * Move temporal expressions to spark-expr crate
    
    * reduce public api
    
    * reduce public api
    
    * update imports in benchmarks
    
    * fmt
    
    * remove unused dep

--- native/core/src/execution/datafusion/expressions/utils.rs
+++ native/core/src/execution/datafusion/expressions/utils.rs
@@ -16,4 +16,4 @@
 // under the License.
 
 // re-export for legacy reasons
-pub use datafusion_comet_spark_expr::utils::{array_with_timezone, down_cast_any_ref};
+pub use datafusion_comet_spark_expr::utils::down_cast_any_ref;
