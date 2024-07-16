565e8c23478a01b659b312271e20aa5b295c7d12
Andy Grove <agrove@apache.org>
2024-07-16 07:07:56 -0600

    Rat

--- benchmarks/join_semi.sql
+++ benchmarks/join_semi.sql
@@ -1,6 +1,22 @@
--- SQLBench-DS query 94 derived from TPC-DS query 94 under the terms of the TPC Fair Use Policy.
--- TPC-DS queries are Copyright 2021 Transaction Processing Performance Council.
--- This query was generated at scale factor 1.
+-- Licensed to the Apache Software Foundation (ASF) under one
+-- or more contributor license agreements.  See the NOTICE file
+-- distributed with this work for additional information
+-- regarding copyright ownership.  The ASF licenses this file
+-- to you under the Apache License, Version 2.0 (the
+-- "License"); you may not use this file except in compliance
+-- with the License.  You may obtain a copy of the License at
+--
+-- http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing,
+-- software distributed under the License is distributed on an
+-- "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+-- KIND, either express or implied.  See the License for the
+-- specific language governing permissions and limitations
+-- under the License.
+
+-- Based on q94
+
 select
     count(distinct ws_order_number) as `order count`
 from
