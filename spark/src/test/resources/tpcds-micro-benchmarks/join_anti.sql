896096aa4de04e8707769f877346790bbb443f00
Andy Grove <agrove@apache.org>
2024-07-17 18:44:12 -0600

    chore: Add microbenchmarks (#671)

---
+++ spark/src/test/resources/tpcds-micro-benchmarks/join_anti.sql
@@ -0,0 +1,29 @@
+-- Licensed to the Apache Software Foundation (ASF) under one
+-- or more contributor license agreements.  See the NOTICE file
+-- distributed with this work for additional information
+-- regarding copyright ownership.  The ASF licenses this file
+-- to you under the Apache License, Version 2.0 (the
+-- "License"); you may not use this file except in compliance
+-- with the License.  You may obtain a copy of the License at
+--
+-- http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing,
+-- software distributed under the License is distributed on an
+-- "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+-- KIND, either express or implied.  See the License for the
+-- specific language governing permissions and limitations
+-- under the License.
+
+-- Based on q94
+
+select
+    count(distinct ws_order_number) as `order count`
+from
+    web_sales ws1
+where not exists(select *
+                 from web_returns wr1
+                 where ws1.ws_order_number = wr1.wr_order_number)
+order by count(distinct ws_order_number)
+    LIMIT 100;
+
---
+++ spark/src/test/resources/tpcds-micro-benchmarks/join_exploding_output.sql
@@ -0,0 +1,22 @@
+-- Licensed to the Apache Software Foundation (ASF) under one
+-- or more contributor license agreements.  See the NOTICE file
+-- distributed with this work for additional information
+-- regarding copyright ownership.  The ASF licenses this file
+-- to you under the Apache License, Version 2.0 (the
+-- "License"); you may not use this file except in compliance
+-- with the License.  You may obtain a copy of the License at
+--
+-- http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing,
+-- software distributed under the License is distributed on an
+-- "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+-- KIND, either express or implied.  See the License for the
+-- specific language governing permissions and limitations
+-- under the License.
+
+-- This is based on the first join in q72 when there is no join reordering
+
+select count(*)
+from catalog_sales join inventory on cs_item_sk = inv_item_sk
+where cs_warehouse_sk = 1 and inv_quantity_on_hand = 666;
