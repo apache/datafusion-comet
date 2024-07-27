22261252a3a466a145bd5844509c7981f5e31917
huaxingao <huaxin.gao11@gmail.com>
2024-07-27 08:52:00 -0700

    fall back to Spark if range offset is not int or long

--- spark/src/main/scala/org/apache/comet/serde/QueryPlanSerde.scala
+++ spark/src/main/scala/org/apache/comet/serde/QueryPlanSerde.scala
@@ -255,9 +255,7 @@ object QueryPlanSerde extends Logging with ShimQueryPlanSerde with CometExprShim
             val offset = e.eval() match {
               case i: Integer => i.toLong
               case l: Long => l
-              case _ =>
-                throw new IllegalArgumentException(
-                  "Unsupported data type for window function row/range offset")
+              case _ => return None
             }
             OperatorOuterClass.LowerWindowFrameBound
               .newBuilder()
@@ -284,9 +282,7 @@ object QueryPlanSerde extends Logging with ShimQueryPlanSerde with CometExprShim
             val offset = e.eval() match {
               case i: Integer => i.toLong
               case l: Long => l
-              case _ =>
-                throw new IllegalArgumentException(
-                  "Unsupported data type for window function row/range offset")
+              case _ => return None
             }
 
             OperatorOuterClass.UpperWindowFrameBound
