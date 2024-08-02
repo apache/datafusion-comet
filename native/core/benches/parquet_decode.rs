98cbec931cdbff2c416f8dee5b1c40b612266445
Kazuyuki Tanimura <ktanimura@apple.com>
2024-08-01 22:19:09 -0700

    fix: Optimize decimal creation macros

--- native/core/benches/parquet_decode.rs
+++ native/core/benches/parquet_decode.rs
@@ -16,13 +16,14 @@
 // under the License.
 
 use arrow_buffer::ToByteSlice;
-use comet::parquet::read::values::{copy_i32_to_i16, copy_i32_to_u16};
+use comet::parquet::read::values::{copy_i32_to_i16, copy_i32_to_u16, copy_i64_to_i64};
 use criterion::{criterion_group, criterion_main, Criterion};
 
 fn criterion_benchmark(c: &mut Criterion) {
     let num = 1000;
-    let source = vec![78_i8; num * 4];
+    let source = vec![78_i8; num * 8];
     let mut group = c.benchmark_group("parquet_decode");
+    /*
     group.bench_function("decode_i32_to_i16", |b| {
         let mut dest: Vec<u8> = vec![b' '; num * 2];
         b.iter(|| {
@@ -33,6 +34,13 @@ fn criterion_benchmark(c: &mut Criterion) {
         let mut dest: Vec<u8> = vec![b' '; num * 4];
         b.iter(|| {
             copy_i32_to_u16(source.to_byte_slice(), dest.as_mut_slice(), num);
+        });
+    });
+     */
+    group.bench_function("decode_i64_to_i64", |b| {
+        let mut dest: Vec<u8> = vec![b' '; num * 8];
+        b.iter(|| {
+            copy_i64_to_i64(source.to_byte_slice(), dest.as_mut_slice(), num);
         });
     });
 }
