a00a6644e46cfbdd67d875dce48c2a784567e239
Andy Grove <agrove@apache.org>
2024-08-20 12:17:44 -0600

    minor: stop warning that AQEShuffleRead cannot run natively (#842)
    * minor: stop warning that AQEShuffleRead cannot run natively
    
    * address feedback
    
    * revert a change
    
    (cherry picked from commit befabdc72ea7e9394e960b678bfbf7897ea12ee0)

--- spark/src/main/scala/org/apache/comet/CometSparkSessionExtensions.scala
+++ spark/src/main/scala/org/apache/comet/CometSparkSessionExtensions.scala
@@ -502,7 +502,7 @@ class CometSparkSessionExtensions
           withInfo(op, "ShuffleHashJoin is not enabled")
           op
 
-        case op: ShuffledHashJoinExec if !op.children.forall(isCometNative(_)) =>
+        case op: ShuffledHashJoinExec if !op.children.forall(isCometNative) =>
           withInfo(
             op,
             "ShuffleHashJoin disabled because the following children are not native " +
@@ -822,15 +822,16 @@ class CometSparkSessionExtensions
           s
 
         case op =>
-          // An operator that is not supported by Comet
           op match {
-            // Broadcast exchange exec is transformed by the parent node. We include
-            // this case specially here so we do not add a misleading 'info' message
-            case _: BroadcastExchangeExec => op
-            case _: CometExec | _: CometBroadcastExchangeExec | _: CometShuffleExchangeExec => op
-            case o =>
-              withInfo(o, s"${o.nodeName} is not supported")
-              o
+            case _: CometExec | _: AQEShuffleReadExec | _: BroadcastExchangeExec |
+                _: CometBroadcastExchangeExec | _: CometShuffleExchangeExec =>
+              // Some execs should never be replaced. We include
+              // these cases specially here so we do not add a misleading 'info' message
+              op
+            case _ =>
+              // An operator that is not supported by Comet
+              withInfo(op, s"${op.nodeName} is not supported")
+              op
           }
       }
     }
