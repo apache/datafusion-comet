61e543b7f0776ab82c826818a897c0b0042084fb
Andy Grove <agrove@apache.org>
2024-08-21 08:34:54 -0600

    rebase on config change PR

--- common/src/main/scala/org/apache/comet/CometConf.scala
+++ common/src/main/scala/org/apache/comet/CometConf.scala
@@ -139,7 +139,7 @@ object CometConf extends ShimCometConf {
   val COMET_EXPR_STDDEV_ENABLED: ConfigEntry[Boolean] =
     createExecEnabledConfig(
       "stddev",
-      defaultValue = true,
+      defaultValue = false,
       notes = Some("stddev is slower than Spark's implementation"))
 
   val COMET_MEMORY_OVERHEAD: OptionalConfigEntry[Long] = conf("spark.comet.memoryOverhead")
