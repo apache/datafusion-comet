7be636fa17fdef069cf0e48052b55cd4476431aa
Andy Grove <agrove@apache.org>
2024-08-26 10:24:11 -0600

    CSV

--- spark/src/test/scala/org/apache/comet/exec/CometExecSuite.scala
+++ spark/src/test/scala/org/apache/comet/exec/CometExecSuite.scala
@@ -1658,6 +1658,20 @@ class CometExecSuite extends CometTestBase {
       })
   }
 
+  test("read CSV file") {
+    Seq("", "csv").foreach { v1List =>
+      withSQLConf(
+        SQLConf.USE_V1_SOURCE_LIST.key -> v1List,
+        CometConf.COMET_EXPLAIN_FALLBACK_ENABLED.key -> "true",
+        CometConf.COMET_CONVERT_FROM_CSV_ENABLED.key -> "true") {
+        spark.read
+          .json("src/test/resources/test-data/csv-test-1.csv")
+          .createOrReplaceTempView("tbl")
+        checkSparkAnswerAndOperator("SELECT * FROM tbl")
+      }
+    }
+  }
+
   test("read JSON file") {
     Seq("", "json").foreach { v1List =>
       withSQLConf(
