f9f32a1d5e0efe5d57e26184d67a645486da61b9
Matt Butrovich <mbutrovich@users.noreply.github.com>
2024-09-03 17:02:15 -0400

    perf: Optimize CometSparkToColumnar for columnar input (#892)
    * Modify `CometSparkToColumnarExec` to use `ColumnarArray` rather than row iteration when the child is producing columnar data.
    
    * Modify `CometSparkToColumnarExec` to use `ColumnarArray` rather than row iteration when the child is producing columnar data.
    
    * Run `mvn spotless:apply` to clean up formatting, changed to camelCase for variables, do not call `ColumnarArray.numElements()` in a tight loop, added `writeColNoNull` as an optimization, reserve the initial vector capacity in `writeCol` as an optimization.
    
    * Fix one more variable name.
    
    * Clean up duplicated code.
    
    * Add test for basic types.
    
    * Add more types to test.
    
    * Revert testing changes to CometExecBenchmark.
    
    * Add benchmark.
    
    * Link to issue in ArrowWriters.
    
    * Address review feedback.
    
    * make format
    
    * Address review feedback, mostly naming suggestions. Ran `make format` again.

--- spark/src/test/scala/org/apache/comet/exec/CometExecSuite.scala
+++ spark/src/test/scala/org/apache/comet/exec/CometExecSuite.scala
@@ -1547,6 +1547,58 @@ class CometExecSuite extends CometTestBase {
     })
   }
 
+  test("SparkToColumnar over BatchScan (Spark Parquet reader)") {
+    Seq("", "parquet").foreach { v1List =>
+      Seq(true, false).foreach { parquetVectorized =>
+        Seq(
+          "cast(id as tinyint)",
+          "cast(id as smallint)",
+          "cast(id as integer)",
+          "cast(id as bigint)",
+          "cast(id as float)",
+          "cast(id as double)",
+          "cast(id as decimal)",
+          "cast(id as timestamp)",
+          "cast(id as string)",
+          "cast(id as binary)",
+          "struct(id)").foreach { valueType =>
+          {
+            withSQLConf(
+              SQLConf.USE_V1_SOURCE_LIST.key -> v1List,
+              CometConf.COMET_NATIVE_SCAN_ENABLED.key -> "false",
+              CometConf.COMET_CONVERT_FROM_PARQUET_ENABLED.key -> "true",
+              SQLConf.PARQUET_VECTORIZED_READER_ENABLED.key -> parquetVectorized.toString) {
+              withTempPath { dir =>
+                var df = spark
+                  .range(10000)
+                  .selectExpr("id as key", s"$valueType as value")
+                  .toDF("key", "value")
+
+                df.write.parquet(dir.toString)
+
+                df = spark.read.parquet(dir.toString)
+                checkSparkAnswerAndOperator(
+                  df.select("*").groupBy("key", "value").count(),
+                  includeClasses = Seq(classOf[CometSparkToColumnarExec]))
+
+                // Verify that the BatchScanExec nodes supported columnar output when requested for Spark 3.4+.
+                // Earlier versions support columnar output for fewer type.
+                if (isSpark34Plus) {
+                  val leaves = df.queryExecution.executedPlan.collectLeaves()
+                  if (parquetVectorized && isSpark34Plus) {
+                    assert(leaves.forall(_.supportsColumnar))
+                  } else {
+                    assert(!leaves.forall(_.supportsColumnar))
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+
   test("SparkToColumnar over InMemoryTableScanExec") {
     Seq("true", "false").foreach(aqe => {
       Seq("true", "false").foreach(cacheVectorized => {
