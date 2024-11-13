8bc00ec57fb4eeaae2df082a14cad4adf215cf0e
NoeB <noe.brehm@bluewin.ch>
2024-11-14 00:57:42 +0100

    feat: support array_append (#1072)
    * feat: support array_append
    
    * formatted code
    
    * rewrite array_append plan to match spark behaviour and fixed bug in QueryPlan serde
    
    * remove unwrap
    
    * Fix for Spark 3.3
    
    * refactor array_append binary expression serde code
    
    * Disabled array_append test for spark 4.0+

--- spark/src/main/scala/org/apache/comet/serde/QueryPlanSerde.scala
+++ spark/src/main/scala/org/apache/comet/serde/QueryPlanSerde.scala
@@ -2237,7 +2237,12 @@ object QueryPlanSerde extends Logging with ShimQueryPlanSerde with CometExprShim
             withInfo(expr, "unsupported arguments for GetArrayStructFields", child)
             None
           }
-
+        case _ if expr.prettyName == "array_append" =>
+          createBinaryExpr(
+            expr.children(0),
+            expr.children(1),
+            inputs,
+            (builder, binaryExpr) => builder.setArrayAppend(binaryExpr))
         case _ =>
           withInfo(expr, s"${expr.prettyName} is not supported", expr.children: _*)
           None
