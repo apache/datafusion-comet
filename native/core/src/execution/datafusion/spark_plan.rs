11b5fb6f10a728d2b692fbac9d1da69f164d1b52
Andy Grove <agrove@apache.org>
2024-12-02 08:51:50 -0700

    address feedback: explain how Spark plan ID is used

--- native/core/src/execution/datafusion/spark_plan.rs
+++ native/core/src/execution/datafusion/spark_plan.rs
@@ -25,9 +25,9 @@ use std::sync::Arc;
 /// for the root plan (such as CopyExec and ScanExec nodes)
 #[derive(Debug, Clone)]
 pub(crate) struct SparkPlan {
-    /// Spark plan ID which is passed down in the protobuf
+    /// Spark plan ID (used for informational purposes only)
     pub(crate) plan_id: u32,
-    /// The root native plan that was generated for this Spark plan
+    /// The root of the native plan that was generated for this Spark plan
     pub(crate) native_plan: Arc<dyn ExecutionPlan>,
     /// Child Spark plans
     pub(crate) children: Vec<Arc<SparkPlan>>,
