5f703d2f0ec23e67333de79aff63a910e712ae18
Matt Butrovich <mbutrovich@gmail.com>
2024-12-04 20:59:35 -0500

    remove errant print.

--- native/core/src/execution/datafusion/planner.rs
+++ native/core/src/execution/datafusion/planner.rs
@@ -2290,7 +2290,6 @@ fn convert_spark_types_to_arrow_schema(
         })
         .collect_vec();
     let arrow_schema: SchemaRef = Arc::new(Schema::new(thing));
-    println!["{}", arrow_schema];
     arrow_schema
 }
 
