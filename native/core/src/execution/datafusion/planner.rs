4638fe3aa5501966cd5d8b53acf26c698b10b3c9
Andy Grove <agrove@apache.org>
2024-12-10 18:47:09 -0700

    remove miri checks

--- native/core/src/execution/datafusion/planner.rs
+++ native/core/src/execution/datafusion/planner.rs
@@ -2504,7 +2504,6 @@ mod tests {
     }
 
     #[test]
-    #[cfg_attr(miri, ignore)] // miri can't call foreign function `rust_psm_stack_pointer`
     fn spark_plan_metrics_filter() {
         let op_scan = create_scan();
         let op = create_filter(op_scan, 0);
@@ -2518,7 +2517,6 @@ mod tests {
     }
 
     #[test]
-    #[cfg_attr(miri, ignore)] // miri can't call foreign function `rust_psm_stack_pointer`
     fn spark_plan_metrics_hash_join() {
         let op_scan = create_scan();
         let op_join = Operator {
