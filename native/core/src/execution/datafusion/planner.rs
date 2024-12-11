93187d0e7947c569aceedea11688abb9db61f749
Andy Grove <agrove@apache.org>
2024-12-11 06:07:02 -0700

    Revert "use return_type_from_exprs"
    This reverts commit febc1f1ec1301f9b359fc23ad6a117224fce35b7.

--- native/core/src/execution/datafusion/planner.rs
+++ native/core/src/execution/datafusion/planner.rs
@@ -2021,11 +2021,9 @@ impl PhysicalPlanner {
                         .coerce_types(&input_expr_types)
                         .unwrap_or_else(|_| input_expr_types.clone());
 
-                    let data_type = func.inner().return_type_from_exprs(
-                        &args,
-                        &input_schema,
-                        &coerced_types,
-                    )?;
+                    // TODO need to call `return_type_from_exprs` instead
+                    #[allow(deprecated)]
+                    let data_type = func.inner().return_type(&coerced_types)?;
 
                     (data_type, coerced_types)
                 }
