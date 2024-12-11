569bb9e99d9a079412948ab0b8f9a8d1226e0949
Kazuyuki Tanimura <ktanimura@apple.com>
2024-12-11 13:31:09 -0800

    reduce internal and external diff (#525)
    This PR reduces some diffs between internal and OSS

--- dev/ensure-jars-have-correct-contents.sh
+++ dev/ensure-jars-have-correct-contents.sh
@@ -95,10 +95,6 @@ allowed_expr+="|^org/apache/spark/CometDriverPlugin.*$"
 allowed_expr+="|^org/apache/spark/CometTaskMemoryManager.class$"
 allowed_expr+="|^org/apache/spark/CometTaskMemoryManager.*$"
 
-allowed_expr+="|^com/$"
-allowed_expr+="|^com/google/$"
-allowed_expr+="|^com/google/flatbuffers/.*$"
-
 allowed_expr+=")"
 declare -i bad_artifacts=0
 declare -a bad_contents
