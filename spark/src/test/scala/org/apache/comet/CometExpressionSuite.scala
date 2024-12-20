988e021593313f72af4d4c027f25d71e7068afe4
Jagdish Parihar <jatin6972@gmail.com>
2024-12-20 16:03:34 +0530

    added comet expression test

--- spark/src/test/scala/org/apache/comet/CometExpressionSuite.scala
+++ spark/src/test/scala/org/apache/comet/CometExpressionSuite.scala
@@ -2522,18 +2522,10 @@ class CometExpressionSuite extends CometTestBase with AdaptiveSparkPlanHelper {
       withTempDir { dir =>
         val path = new Path(dir.toURI.toString, "test.parquet")
         makeParquetFileAllTypes(path, dictionaryEnabled, 10000)
-
-        // Test basic array_remove functionality
-        checkSparkAnswerAndOperator(sql("SELECT array_remove(array(1, 2, 3, null, 3), 2)"))
-
-        // Test removing multiple occurrences
-        checkSparkAnswerAndOperator(sql("SELECT array_remove(array(1, 3, 3, null, 3), 3)"))
-
-        // Test removing null
-        checkSparkAnswerAndOperator(sql("SELECT array_remove(array(1, 2, null, 4, null), null)"))
-
-        // Test when element doesn't exist
-        checkSparkAnswerAndOperator(sql("SELECT array_remove(array(1, 2, 3), 5)"))
+        spark.read.parquet(path.toString).createOrReplaceTempView("t1");
+        checkSparkAnswerAndOperator(sql("SELECT array_remove(array(_2, _3,_4), 2) from t1"))
+        checkSparkAnswerAndOperator(sql("SELECT array_remove(array(_2, _3,_4), 3) from t1"))
+        checkSparkAnswerAndOperator(sql("SELECT array_remove(array(_2, _3,_4), 5) from t1"))
       }
     }
   }
