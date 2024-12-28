466400c7acc2ae55ac3293cfad6a0d7072105bbd
Andy Grove <agrove@apache.org>
2024-12-28 12:14:49 -0700

    revert miri change

--- native/core/src/execution/planner.rs
+++ native/core/src/execution/planner.rs
@@ -2513,7 +2513,6 @@ mod tests {
     }
 
     #[test]
-    #[cfg_attr(miri, ignore)] // miri can't call foreign function `rust_psm_stack_pointer`
     fn spark_plan_metrics_filter() {
         let op_scan = create_scan();
         let op = create_filter(op_scan, 0);
@@ -2527,7 +2526,6 @@ mod tests {
     }
 
     #[test]
-    #[cfg_attr(miri, ignore)] // miri can't call foreign function `rust_psm_stack_pointer`
     fn spark_plan_metrics_hash_join() {
         let op_scan = create_scan();
         let op_join = Operator {
