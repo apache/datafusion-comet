de3d5997b61e0d911f82419c8696fa06765aa7cb
Jagdish Parihar <jatin6972@gmail.com>
2025-01-08 00:14:22 +0530

    updated the test for 'array_remove'

--- spark/src/test/scala/org/apache/comet/CometExpressionSuite.scala
+++ spark/src/test/scala/org/apache/comet/CometExpressionSuite.scala
@@ -2535,8 +2535,10 @@ class CometExpressionSuite extends CometTestBase with AdaptiveSparkPlanHelper {
         val path = new Path(dir.toURI.toString, "test.parquet")
         makeParquetFileAllTypes(path, dictionaryEnabled, 10000)
         spark.read.parquet(path.toString).createOrReplaceTempView("t1");
-        checkSparkAnswerAndOperator(sql("SELECT array_remove(array(_2, _3,_4), _2) from t1"))
-        checkSparkAnswerAndOperator(sql("SELECT array_remove(array(_2, _3,_4), 3) from t1"))
+        checkSparkAnswerAndOperator(
+          sql("SELECT array_remove(array(_2, _3,_4), _2) from t1 where _2 is null"))
+        checkSparkAnswerAndOperator(
+          sql("SELECT array_remove(array(_2, _3,_4), _3) from t1 where _3 is not null"))
         checkSparkAnswerAndOperator(sql("SELECT array_remove(array(_2, _3,_4), 10) from t1"))
       }
     }
