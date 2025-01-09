ef85f88c6ae81adbddd53de429d2862b19bd86e8
Andy Grove <agrove@apache.org>
2025-01-09 16:59:56 -0700

    perf: Improve query planning to more reliably fall back to columnar shuffle when native shuffle is not supported (#1209)

--- spark/src/test/scala/org/apache/spark/sql/comet/CometPlanStabilitySuite.scala
+++ spark/src/test/scala/org/apache/spark/sql/comet/CometPlanStabilitySuite.scala
@@ -263,6 +263,7 @@ trait CometPlanStabilitySuite extends DisableAdaptiveExecutionSuite with TPCDSBa
       CometConf.COMET_EXEC_ENABLED.key -> "true",
       CometConf.COMET_DPP_FALLBACK_ENABLED.key -> "false",
       CometConf.COMET_EXEC_SHUFFLE_ENABLED.key -> "true",
+      CometConf.COMET_SHUFFLE_FALLBACK_TO_COLUMNAR.key -> "true",
       CometConf.COMET_EXEC_SORT_MERGE_JOIN_WITH_JOIN_FILTER_ENABLED.key -> "true",
       CometConf.COMET_CAST_ALLOW_INCOMPATIBLE.key -> "true", // needed for v1.4/q9, v1.4/q44, v2.7.0/q6, v2.7.0/q64
       "spark.sql.readSideCharPadding" -> "false",
