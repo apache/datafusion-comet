a8a4da99cb5a06b4b2e9eda821b1f16168d23925
wforget <643348094@qq.com>
2025-01-14 21:35:23 +0800

    fix style

--- spark/src/test/scala/org/apache/comet/CometCastSuite.scala
+++ spark/src/test/scala/org/apache/comet/CometCastSuite.scala
@@ -839,7 +839,9 @@ class CometCastSuite extends CometTestBase with AdaptiveSparkPlanHelper {
   }
 
   test("cast TimestampType to DecimalType(10,2) is unsupported") {
-    castFallbackTest(generateTimestamps(), DataTypes.createDecimalType(10, 2),
+    castFallbackTest(
+      generateTimestamps(),
+      DataTypes.createDecimalType(10, 2),
       "Unsupported cast from TimestampType to DecimalType(10,2)")
   }
 
