4fe4f57da6339a5d66114750aee915525e601d4d
KAZUYUKI TANIMURA <ktanimura@apple.com>
2025-02-12 10:17:02 -0800

    feat: Add unbounded memory pool (#1386)
    ## Which issue does this PR close?
    
    ## Rationale for this change
    
    DataFusion has an unbounded memory pool. I found it useful for experimental purpose.
    
    ## What changes are included in this PR?
    
    Added an option for unbounded memory pool.
    
    ## How are these changes tested?
    
    existing tests

--- common/src/main/scala/org/apache/comet/CometConf.scala
+++ common/src/main/scala/org/apache/comet/CometConf.scala
@@ -504,8 +504,8 @@ object CometConf extends ShimCometConf {
     .doc(
       "The type of memory pool to be used for Comet native execution. " +
         "Available memory pool types are 'greedy', 'fair_spill', 'greedy_task_shared', " +
-        "'fair_spill_task_shared', 'greedy_global' and 'fair_spill_global'. For off-heap " +
-        "types are 'unified' and `fair_unified`.")
+        "'fair_spill_task_shared', 'greedy_global', 'fair_spill_global', and `unbounded`. " +
+        "For off-heap types are 'unified' and `fair_unified`.")
     .stringConf
     .createWithDefault("greedy_task_shared")
 
