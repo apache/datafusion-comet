a7ab8d832ea772c5ebc8281cffd94ab6ba3433ef
Matt Butrovich <mbutrovich@gmail.com>
2025-03-28 15:28:30 -0400

    Passes test, maybe revert `with DataSourceScanExec` since I don't know the full implications of that.

--- spark/src/test/scala/org/apache/comet/exec/CometExecSuite.scala
+++ spark/src/test/scala/org/apache/comet/exec/CometExecSuite.scala
@@ -813,11 +813,6 @@ class CometExecSuite extends CometTestBase {
   }
 
   test("explain native plan") {
-    // https://github.com/apache/datafusion-comet/issues/1441
-    assume(!CometConf.isExperimentalNativeScan)
-    // there are no assertions in this test to prove that the explain feature
-    // wrote the expected output to stdout, but we at least test that enabling
-    // the config does not cause any exceptions.
     withSQLConf(
       CometConf.COMET_EXPLAIN_NATIVE_ENABLED.key -> "true",
       SQLConf.AUTO_BROADCASTJOIN_THRESHOLD.key -> "-1") {
