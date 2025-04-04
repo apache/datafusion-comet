c57ba4f39f230e8003be06b738b74ea0c0f8ce6a
Andy Grove <agrove@apache.org>
2025-04-04 13:56:09 -0600

    Save

--- spark/src/test/scala/org/apache/comet/CometFuzzTestSuite.scala
+++ spark/src/test/scala/org/apache/comet/CometFuzzTestSuite.scala
@@ -69,14 +69,11 @@ class CometFuzzTestSuite extends CometTestBase with AdaptiveSparkPlanHelper {
   // TODO fails with Unsupported data type org.apache.spark.sql.Row
   // https://github.com/apache/datafusion-comet/issues/1611
   ignore("aggregate group by single column") {
-    // scalastyle:off println
     val df = spark.read.parquet(filename)
     df.createOrReplaceTempView("t1")
     for (field <- df.schema.fields) {
-      println(field.dataType)
       val col = field.name
       val sql = s"SELECT $col, count(*) FROM t1 GROUP BY $col ORDER BY $col"
-      println(spark.sql(sql).queryExecution.executedPlan)
       checkSparkAnswer(sql)
     }
   }
