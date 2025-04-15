0ba03a1d55f02bb6539b52f6644d4f221a109abe
Parth Chandra <parthc@apache.org>
2025-04-15 08:20:54 -0700

    fix: fix spark/sql test failures in native_iceberg_compat (#1593)

--- common/src/main/java/org/apache/comet/parquet/BatchReader.java
+++ common/src/main/java/org/apache/comet/parquet/BatchReader.java
@@ -23,10 +23,7 @@ import java.io.Closeable;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Future;
@@ -285,8 +282,8 @@ public class BatchReader extends RecordReader<Void, ColumnarBatch> implements Cl
     // Initialize missing columns and use null vectors for them
     missingColumns = new boolean[columns.size()];
     List<String[]> paths = requestedSchema.getPaths();
-    StructField[] nonPartitionFields = sparkSchema.fields();
     ShimFileFormat.findRowIndexColumnIndexInSchema(sparkSchema);
+    StructField[] nonPartitionFields = sparkSchema.fields();
     for (int i = 0; i < requestedSchema.getFieldCount(); i++) {
       Type t = requestedSchema.getFields().get(i);
       Preconditions.checkState(
