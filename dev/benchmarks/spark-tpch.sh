33c7884f36b21bc03329630da927e4caa3d3fd4b
Andy Grove <agrove@apache.org>
2025-04-15 13:36:53 -0400

    save

--- dev/benchmarks/spark-tpch.sh
+++ dev/benchmarks/spark-tpch.sh
@@ -11,7 +11,7 @@ $SPARK_HOME/bin/spark-submit \
     --conf spark.executor.memory=8g \
     --conf spark.memory.offHeap.enabled=true \
     --conf spark.memory.offHeap.size=8g \
-    /home/ec2-user/datafusion-benchmarks/runners/datafusion-comet/tpcbench.py \
+    tpcbench.py \
     --name spark \
     --benchmark tpch \
     --data /home/ec2-user/ \
