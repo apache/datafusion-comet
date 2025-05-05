a89a9dcbbec7463f369b64cc4bc50d0c5a92a9a5
Andy Grove <agrove@apache.org>
2025-05-05 10:31:55 -0600

    address feedback

--- dev/benchmarks/setup.sh
+++ dev/benchmarks/setup.sh
@@ -34,11 +34,17 @@ make release PROFILES="-Pspark-3.5"
 git clone https://github.com/apache/datafusion-benchmarks.git
 
 # Install Spark
-mkdir /home/ec2-user/tmp
 wget https://dlcdn.apache.org/spark/spark-3.5.5/spark-3.5.5-bin-hadoop3.tgz
-tar xzf spark-3.5.5-bin-hadoop3.tgz
-cp spark-env.sh spark-3.5.5-bin-hadoop3/conf
-export SPARK_HOME=/home/ec2-user/spark-3.5.5-bin-hadoop3/
+if [ ! -d "spark-3.5.5-bin-hadoop3" ]; then
+  wget https://dlcdn.apache.org/spark/spark-3.5.5/spark-3.5.5-bin-hadoop3.tgz
+  tar xzf spark-3.5.5-bin-hadoop3.tgz
+fi
+
+# Create a temp folder on the primary EBS volume
+mkdir /home/ec2-user/tmp
+
+# Start Spark
+export SPARK_HOME=/home/ec2-user/spark-3.5.5-bin-hadoop3
 export SPARK_MASTER=spark://localhost:7077
 $SPARK_HOME/sbin/start-master.sh --host localhost
 $SPARK_HOME/sbin/start-worker.sh $SPARK_MASTER
