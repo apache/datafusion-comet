610f0fff6f1d824851594d19413aac7dbf86cd94
Zhen Wang <643348094@qq.com>
2025-05-13 06:52:23 +0800

    fix: Check acquired memory when CometMemoryPool grows (#1732)

--- spark/src/main/java/org/apache/spark/CometTaskMemoryManager.java
+++ spark/src/main/java/org/apache/spark/CometTaskMemoryManager.java
@@ -48,6 +48,10 @@ public class CometTaskMemoryManager {
   // Returns the actual amount of memory (in bytes) granted.
   public long acquireMemory(long size) {
     long acquired = internal.acquireExecutionMemory(size, nativeMemoryConsumer);
+    if (acquired < size) {
+      // If memory manager is not able to acquire the requested size, log memory usage
+      internal.showMemoryUsage();
+    }
     used += acquired;
     return acquired;
   }
