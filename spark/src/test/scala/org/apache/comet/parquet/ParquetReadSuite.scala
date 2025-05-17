e6ae57b25007adef1c05b111207ad51cf032be31
Andy Grove <agrove@apache.org>
2025-05-17 14:48:37 -0600

    scalastyle

--- spark/src/test/scala/org/apache/comet/parquet/ParquetReadSuite.scala
+++ spark/src/test/scala/org/apache/comet/parquet/ParquetReadSuite.scala
@@ -48,7 +48,6 @@ import org.apache.spark.unsafe.types.UTF8String
 import com.google.common.primitives.UnsignedLong
 
 import org.apache.comet.CometConf
-import org.apache.comet.CometConf.SCAN_NATIVE_ICEBERG_COMPAT
 import org.apache.comet.CometSparkSessionExtensions.isSpark40Plus
 import org.apache.comet.rules.CometScanTypeChecker
 
