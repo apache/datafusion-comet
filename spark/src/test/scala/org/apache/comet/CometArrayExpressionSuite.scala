5591f8e4c7b4a4e9103eb87280af53d4d9d4b718
Andy Grove <agrove@apache.org>
2025-05-22 08:44:04 -0600

    ignore array_repeat test

--- spark/src/test/scala/org/apache/comet/CometArrayExpressionSuite.scala
+++ spark/src/test/scala/org/apache/comet/CometArrayExpressionSuite.scala
@@ -410,7 +410,7 @@ class CometArrayExpressionSuite extends CometTestBase with AdaptiveSparkPlanHelp
     }
   }
 
-  test("array_repeat") {
+  ignore("array_repeat") {
     withSQLConf(
       CometConf.COMET_EXPR_ALLOW_INCOMPATIBLE.key -> "true",
       CometConf.COMET_EXPLAIN_FALLBACK_ENABLED.key -> "true") {
