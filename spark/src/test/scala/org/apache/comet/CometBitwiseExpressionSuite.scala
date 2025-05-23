bad92ee41754fd91cf465dcf513a2b55e78b12cd
Kazantsev Maksim <mn.kazantsev@gmail.com>
2025-05-23 21:44:54 +0400

    Fix tests

--- spark/src/test/scala/org/apache/comet/CometBitwiseExpressionSuite.scala
+++ spark/src/test/scala/org/apache/comet/CometBitwiseExpressionSuite.scala
@@ -134,7 +134,6 @@ class CometBitwiseExpressionSuite extends CometTestBase with AdaptiveSparkPlanHe
                 s"bit_get(_3, $shortBitPosition)",
                 s"bit_get(_4, $intBitPosition)",
                 s"bit_get(_5, $longBitPosition)",
-                s"bit_get(_10, $intBitPosition)",
                 s"bit_get(_11, $longBitPosition)"))
         }
       }
