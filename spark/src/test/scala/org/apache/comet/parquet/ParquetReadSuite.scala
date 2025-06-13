de348ca0cfe368928241642f65a9b7e96706b6bb
Parth Chandra <parthc@apache.org>
2025-06-13 13:54:00 -0700

    address review comment

--- spark/src/test/scala/org/apache/comet/parquet/ParquetReadSuite.scala
+++ spark/src/test/scala/org/apache/comet/parquet/ParquetReadSuite.scala
@@ -1800,7 +1800,7 @@ abstract class ParquetReadSuite extends CometTestBase {
         Row(null) ::
         Nil
 
-    withSQLConf("spark.sql.parquet.fieldId.read.enabled" -> "true") {
+    withSQLConf(SQLConf.PARQUET_FIELD_ID_READ_ENABLED.key  -> "true") {
       val df = spark.createDataFrame(spark.sparkContext.parallelize(data), nestedSchema)
       withTempPath { path =>
         df.write.parquet(path.getCanonicalPath)
