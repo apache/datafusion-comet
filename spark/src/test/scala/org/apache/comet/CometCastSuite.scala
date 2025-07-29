7c650a7475b87a8bc7718891a70a0e6c73f0a1b1
Parth Chandra <parthc@apache.org>
2025-07-28 17:56:44 -0700

    fix style

--- spark/src/test/scala/org/apache/comet/CometCastSuite.scala
+++ spark/src/test/scala/org/apache/comet/CometCastSuite.scala
@@ -1066,7 +1066,7 @@ class CometCastSuite extends CometTestBase with AdaptiveSparkPlanHelper {
   test("test implicit cast to dictionary with case when and dictionary type") {
     withSQLConf("parquet.enable.dictionary" -> "true") {
       withParquetTable((0 until 10000).map(i => (i < 5000, "one")), "tbl") {
-        val df = spark.sql(s"select case when (_1 = true) then _2 else '' end as aaa from tbl")
+        val df = spark.sql("select case when (_1 = true) then _2 else '' end as aaa from tbl")
         checkSparkAnswerAndOperator(df)
       }
     }
