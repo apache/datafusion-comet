89b8270b25740c0fd0e7527ad5dda7eeb87bbb83
Andy Grove <agrove@apache.org>
2025-08-14 18:08:41 -0600

    remove unused method

--- spark/src/main/scala/org/apache/comet/CometSparkSessionExtensions.scala
+++ spark/src/main/scala/org/apache/comet/CometSparkSessionExtensions.scala
@@ -145,16 +145,6 @@ object CometSparkSessionExtensions extends Logging {
   private[comet] def isCometShuffleEnabled(conf: SQLConf): Boolean =
     COMET_EXEC_SHUFFLE_ENABLED.get(conf) && isCometShuffleManagerEnabled(conf)
 
-  private[comet] def getCometShuffleNotEnabledReason(conf: SQLConf): Option[String] = {
-    if (!COMET_EXEC_SHUFFLE_ENABLED.get(conf)) {
-      Some(s"${COMET_EXEC_SHUFFLE_ENABLED.key} is not enabled")
-    } else if (!isCometShuffleManagerEnabled(conf)) {
-      Some(s"spark.shuffle.manager is not set to ${CometShuffleManager.getClass.getName}")
-    } else {
-      None
-    }
-  }
-
   private def isCometShuffleManagerEnabled(conf: SQLConf) = {
     conf.contains("spark.shuffle.manager") && conf.getConfString("spark.shuffle.manager") ==
       "org.apache.spark.sql.comet.execution.shuffle.CometShuffleManager"
