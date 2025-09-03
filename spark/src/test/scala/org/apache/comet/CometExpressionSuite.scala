ed85ea6cf56f194159b785e81c3748a10744b2f0
Bhargava Vadlamani <fullcircle289@gmail.com>
2025-09-03 00:32:33 -0700

    address_review_comments

--- spark/src/test/scala/org/apache/comet/CometExpressionSuite.scala
+++ spark/src/test/scala/org/apache/comet/CometExpressionSuite.scala
@@ -322,7 +322,7 @@ class CometExpressionSuite extends CometTestBase with AdaptiveSparkPlanHelper {
       checkSparkAnswer("SELECT try_add(_1, _2) FROM tbl")
     }
   }
-  test("fix_rpad") {
+  test("Verify rpad expr support for second arg instead of just literal") {
     withTable("t1") {
       val value = "IfIWasARoadIWouldBeBent"
       sql("create table t1(c1 varchar(100), c2 int) using parquet")
