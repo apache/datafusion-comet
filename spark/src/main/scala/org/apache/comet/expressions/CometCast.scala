39c527e085090099103833b7953afd132b9b24ce
Andy Grove <agrove@apache.org>
2025-09-07 08:49:11 -0600

    revert cast

--- spark/src/main/scala/org/apache/comet/expressions/CometCast.scala
+++ spark/src/main/scala/org/apache/comet/expressions/CometCast.scala
@@ -48,10 +48,6 @@ object CometCast extends CometExpressionSerde[Cast] with CometExprShim {
   // TODO add DataTypes.TimestampNTZType for Spark 3.4 and later
   // https://github.com/apache/datafusion-comet/issues/378
 
-  override def getExprConfigName(expr: Cast): String = {
-    s"Cast${expr.child.dataType}To${expr.dataType}"
-  }
-
   override def getSupportLevel(cast: Cast): SupportLevel = {
     isSupported(cast.child.dataType, cast.dataType, cast.timeZoneId, evalMode(cast))
   }
