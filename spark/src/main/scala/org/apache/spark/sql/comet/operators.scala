1d4cccfebc6c21ced7380019ad0884ebd7a2d05a
wforget <643348094@qq.com>
2025-09-15 18:13:32 +0800

    thread-safety

--- spark/src/main/scala/org/apache/spark/sql/comet/operators.scala
+++ spark/src/main/scala/org/apache/spark/sql/comet/operators.scala
@@ -274,16 +274,28 @@ abstract class CometNativeExec extends CometExec {
         sparkPlans.zipWithIndex.foreach { case (plan, idx) =>
           plan match {
             case c: CometBroadcastExchangeExec =>
-              inputs += c.setNumPartitions(firstNonBroadcastPlanNumPartitions).executeColumnar()
+              inputs += c
+                .executeColumnar()
+                .asInstanceOf[CometBatchRDD]
+                .withNumPartitions(firstNonBroadcastPlanNumPartitions)
             case BroadcastQueryStageExec(_, c: CometBroadcastExchangeExec, _) =>
-              inputs += c.setNumPartitions(firstNonBroadcastPlanNumPartitions).executeColumnar()
+              inputs += c
+                .executeColumnar()
+                .asInstanceOf[CometBatchRDD]
+                .withNumPartitions(firstNonBroadcastPlanNumPartitions)
             case ReusedExchangeExec(_, c: CometBroadcastExchangeExec) =>
-              inputs += c.setNumPartitions(firstNonBroadcastPlanNumPartitions).executeColumnar()
+              inputs += c
+                .executeColumnar()
+                .asInstanceOf[CometBatchRDD]
+                .withNumPartitions(firstNonBroadcastPlanNumPartitions)
             case BroadcastQueryStageExec(
                   _,
                   ReusedExchangeExec(_, c: CometBroadcastExchangeExec),
                   _) =>
-              inputs += c.setNumPartitions(firstNonBroadcastPlanNumPartitions).executeColumnar()
+              inputs += c
+                .executeColumnar()
+                .asInstanceOf[CometBatchRDD]
+                .withNumPartitions(firstNonBroadcastPlanNumPartitions)
             case _: CometNativeExec =>
             // no-op
             case _ if idx == firstNonBroadcastPlan.get._2 =>
