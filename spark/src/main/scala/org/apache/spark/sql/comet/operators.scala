e2b0253d98573bea9f072c087b238a7ba30c4fd3
comphead <comphead@ukr.net>
2025-09-16 20:11:00 -0700

    fix: correct `missingInput` for `CometHashAggregateExec`

--- spark/src/main/scala/org/apache/spark/sql/comet/operators.scala
+++ spark/src/main/scala/org/apache/spark/sql/comet/operators.scala
@@ -692,6 +692,8 @@ case class CometHashAggregateExec(
     override val serializedPlanOpt: SerializedPlan)
     extends CometUnaryExec
     with PartitioningPreservingUnaryExecNode {
+  override def producedAttributes: AttributeSet = outputSet ++ AttributeSet(resultExpressions)
+
   override protected def withNewChildInternal(newChild: SparkPlan): SparkPlan =
     this.copy(child = newChild)
 
