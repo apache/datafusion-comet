506def74d3e9bec5566a12827a1e5537d4669e62
Andy Grove <agrove@apache.org>
2025-10-23 09:32:23 -0600

    add option to write query results to Parquet

--- dev/benchmarks/tpcbench.py
+++ dev/benchmarks/tpcbench.py
@@ -91,7 +91,7 @@ def main(benchmark: str, data_path: str, query_path: str, iterations: int, outpu
                         df.explain()
 
                         if write_path is not None:
-                            output_path = f"{write_path}/{query}.parquet"
+                            output_path = f"{write_path}/q{query}.parquet"
                             df.write.parquet(output_path)
                             print(f"Query {query} results written to {output_path}")
                         else:
