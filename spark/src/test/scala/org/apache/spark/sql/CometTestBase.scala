eec87b1fcc5641a2ab9cf0704bf75fe5ab02e555
Andy Grove <agrove@apache.org>
2025-10-23 09:24:06 -0600

    remove debug println

--- spark/src/test/scala/org/apache/spark/sql/CometTestBase.scala
+++ spark/src/test/scala/org/apache/spark/sql/CometTestBase.scala
@@ -180,8 +180,6 @@ abstract class CometTestBase
       df: => DataFrame,
       includeClasses: Seq[Class[_]],
       excludedClasses: Class[_]*): Unit = {
-    // scalastyle:off
-    println(df.queryExecution.executedPlan)
     checkCometOperators(stripAQEPlan(df.queryExecution.executedPlan), excludedClasses: _*)
     checkPlanContains(stripAQEPlan(df.queryExecution.executedPlan), includeClasses: _*)
     checkSparkAnswer(df)
