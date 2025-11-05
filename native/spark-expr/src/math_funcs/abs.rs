437cfdcfc225c7550a9722e2a0ac36ef68f441e8
Andy Grove <agrove@apache.org>
2025-11-05 12:42:30 -0700

    Update native/spark-expr/src/math_funcs/abs.rs
    Co-authored-by: Oleks V <comphead@users.noreply.github.com>

--- native/spark-expr/src/math_funcs/abs.rs
+++ native/spark-expr/src/math_funcs/abs.rs
@@ -362,7 +362,7 @@ mod tests {
                         );
                         Ok(())
                     } else {
-                        panic!("Didn't expect error, but got: {e:?}")
+                        unreachable!("Didn't expect error, but got: {e:?}")
                     }
                 }
                 _ => unreachable!(),
