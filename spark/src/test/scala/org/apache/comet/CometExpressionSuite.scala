7f6c9eab878f3573b3393c2d0d7158a8b0e7bee2
Vrishabh <psvrishabh@gmail.com>
2025-11-10 17:09:53 +0000

    feat: support hyperbolic functions

--- spark/src/test/scala/org/apache/comet/CometExpressionSuite.scala
+++ spark/src/test/scala/org/apache/comet/CometExpressionSuite.scala
@@ -1346,13 +1346,16 @@ class CometExpressionSuite extends CometTestBase with AdaptiveSparkPlanHelper {
           "asin",
           "atan",
           "cos",
+          "cosh",
           "exp",
           "ln",
           "log10",
           "log2",
           "sin",
+          "sinh",
           "sqrt",
           "tan",
+          "tanh",
           "cot")) {
         val (_, cometPlan) =
           checkSparkAnswerAndOperatorWithTol(sql(s"SELECT $expr(_1), $expr(_2) FROM tbl"))
