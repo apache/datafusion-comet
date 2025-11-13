c9b77a2aa532a28d936314478d8496f9a23625bc
Andy Grove <agrove@apache.org>
2025-11-13 16:04:46 -0700

    style

--- common/src/main/scala/org/apache/comet/CometConf.scala
+++ common/src/main/scala/org/apache/comet/CometConf.scala
@@ -677,8 +677,9 @@ object CometConf extends ShimCometConf {
   val COMET_EXEC_STRICT_FLOATING_POINT: ConfigEntry[Boolean] =
     conf("spark.comet.exec.strictFloatingPoint")
       .category(CATEGORY_EXEC)
-      .doc("When enabled, fall back to Spark for floating-point operations that may differ from " +
-        s"Spark, such as when comparing or sorting -0.0 and 0.0. $COMPAT_GUIDE.")
+      .doc(
+        "When enabled, fall back to Spark for floating-point operations that may differ from " +
+          s"Spark, such as when comparing or sorting -0.0 and 0.0. $COMPAT_GUIDE.")
       .booleanConf
       .createWithDefault(false)
 
