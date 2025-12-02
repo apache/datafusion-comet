f9dc3e5b3070d0eeb1ee22b2967e1ac5cb483571
Andy Grove <agrove@apache.org>
2025-12-02 14:36:17 -0700

    Revert

--- spark/src/test/scala/org/apache/spark/sql/benchmark/CometTPCDSMicroBenchmark.scala
+++ spark/src/test/scala/org/apache/spark/sql/benchmark/CometTPCDSMicroBenchmark.scala
@@ -52,7 +52,32 @@ import org.apache.comet.CometConf
  */
 object CometTPCDSMicroBenchmark extends CometTPCQueryBenchmarkBase {
 
-  val queries: Seq[String] = Seq("explode")
+  val queries: Seq[String] = Seq(
+    "scan_decimal",
+    "add_decimals",
+    "add_many_decimals",
+    "add_many_integers",
+    "agg_high_cardinality",
+    "agg_low_cardinality",
+    "agg_sum_decimals_no_grouping",
+    "agg_sum_integers_no_grouping",
+    "agg_sum_integers_with_grouping",
+    "agg_stddev",
+    "case_when_column_or_null",
+    "case_when_scalar",
+    "char_type",
+    "filter_highly_selective",
+    "filter_less_selective",
+    "if_column_or_null",
+    "join_anti",
+    "join_condition",
+    "join_exploding_output",
+    "join_inner",
+    "join_left_outer",
+    "join_semi",
+    "rlike",
+    "explode",
+    "to_json")
 
   override def runQueries(
       queryLocation: String,
