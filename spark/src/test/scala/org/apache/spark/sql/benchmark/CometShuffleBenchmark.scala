5cf46c1ef5919654a1bf332d1a91498e081dc8f0
Andy Grove <agrove@apache.org>
2025-12-13 12:40:01 -0700

    fix

--- spark/src/test/scala/org/apache/spark/sql/benchmark/CometShuffleBenchmark.scala
+++ spark/src/test/scala/org/apache/spark/sql/benchmark/CometShuffleBenchmark.scala
@@ -510,71 +510,68 @@ object CometShuffleBenchmark extends CometBenchmarkBase {
     for (generateArray <- Seq(true, false)) {
       for (generateStruct <- Seq(true, false)) {
         if (generateArray || generateStruct) {
-          for (maxDepth <- Seq(2, 4)) {
-            val filename =
-              createDeeplyNestedParquetFile(numRows, maxDepth, generateArray, generateStruct)
-            try {
-              val df = spark.read.parquet(filename)
+          val filename =
+            createDeeplyNestedParquetFile(numRows, maxDepth, generateArray, generateStruct)
+          try {
+            val df = spark.read.parquet(filename)
 
-              withTempTable("deeplyNestedTable") {
-                df.createOrReplaceTempView("deeplyNestedTable")
+            withTempTable("deeplyNestedTable") {
+              df.createOrReplaceTempView("deeplyNestedTable")
 
-                val sql = "select * from deeplyNestedTable"
-                val config = s"arrays=$generateArray, structs=$generateStruct, maxDepth=$maxDepth"
+              val sql = "select * from deeplyNestedTable"
+              val config = s"arrays=$generateArray, structs=$generateStruct, maxDepth=$maxDepth"
 
-                benchmark.addCase(s"SQL Parquet ($config) - Spark") { _ =>
+              benchmark.addCase(s"SQL Parquet ($config) - Spark") { _ =>
+                spark
+                  .sql(sql)
+                  .repartition(partitionNum)
+                  .noop()
+              }
+
+              benchmark.addCase(s"SQL Parquet ($config) - Comet (Spark Shuffle)") { _ =>
+                withSQLConf(
+                  CometConf.COMET_ENABLED.key -> "true",
+                  CometConf.COMET_EXEC_ENABLED.key -> "true",
+                  CometConf.COMET_EXEC_SHUFFLE_ENABLED.key -> "false") {
                   spark
                     .sql(sql)
                     .repartition(partitionNum)
                     .noop()
                 }
+              }
 
-                benchmark.addCase(s"SQL Parquet ($config) - Comet (Spark Shuffle)") { _ =>
-                  withSQLConf(
-                    CometConf.COMET_ENABLED.key -> "true",
-                    CometConf.COMET_EXEC_ENABLED.key -> "true",
-                    CometConf.COMET_EXEC_SHUFFLE_ENABLED.key -> "false") {
-                    spark
-                      .sql(sql)
-                      .repartition(partitionNum)
-                      .noop()
-                  }
+              benchmark.addCase(s"SQL Parquet ($config) - Comet (JVM Shuffle)") { _ =>
+                withSQLConf(
+                  CometConf.COMET_ENABLED.key -> "true",
+                  CometConf.COMET_EXEC_ENABLED.key -> "true",
+                  CometConf.COMET_EXEC_SHUFFLE_ENABLED.key -> "true",
+                  CometConf.COMET_SHUFFLE_MODE.key -> "jvm") {
+                  spark
+                    .sql(sql)
+                    .repartition(partitionNum)
+                    .noop()
                 }
+              }
 
-                benchmark.addCase(s"SQL Parquet ($config) - Comet (JVM Shuffle)") { _ =>
-                  withSQLConf(
-                    CometConf.COMET_ENABLED.key -> "true",
-                    CometConf.COMET_EXEC_ENABLED.key -> "true",
-                    CometConf.COMET_EXEC_SHUFFLE_ENABLED.key -> "true",
-                    CometConf.COMET_SHUFFLE_MODE.key -> "jvm") {
-                    spark
-                      .sql(sql)
-                      .repartition(partitionNum)
-                      .noop()
-                  }
-                }
-
-                benchmark.addCase(s"SQL Parquet ($config) - Comet (Native Shuffle)") { _ =>
-                  withSQLConf(
-                    CometConf.COMET_ENABLED.key -> "true",
-                    CometConf.COMET_EXEC_ENABLED.key -> "true",
-                    CometConf.COMET_EXEC_SHUFFLE_ENABLED.key -> "true",
-                    CometConf.COMET_SHUFFLE_MODE.key -> "native") {
-                    spark
-                      .sql(sql)
-                      .repartition(partitionNum)
-                      .noop()
-                  }
+              benchmark.addCase(s"SQL Parquet ($config) - Comet (Native Shuffle)") { _ =>
+                withSQLConf(
+                  CometConf.COMET_ENABLED.key -> "true",
+                  CometConf.COMET_EXEC_ENABLED.key -> "true",
+                  CometConf.COMET_EXEC_SHUFFLE_ENABLED.key -> "true",
+                  CometConf.COMET_SHUFFLE_MODE.key -> "native") {
+                  spark
+                    .sql(sql)
+                    .repartition(partitionNum)
+                    .noop()
                 }
                 benchmark.run()
               }
-            } finally {
-              new java.io.File(filename).delete()
             }
+          } finally {
+            new java.io.File(filename).delete()
           }
         }
       }
-
     }
   }
 
