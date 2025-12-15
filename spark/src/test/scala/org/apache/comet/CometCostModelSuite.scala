bc2ced7f786f57ebab6a6cd57713b7b19151b17d
Andy Grove <agrove@apache.org>
2025-12-14 17:23:13 -0700

    test

--- spark/src/test/scala/org/apache/comet/CometCostModelSuite.scala
+++ spark/src/test/scala/org/apache/comet/CometCostModelSuite.scala
@@ -37,146 +37,121 @@ class CometCostModelSuite extends CometTestBase {
   // - ascii: 0.6x -> cost = 1/0.6 = 1.67 (high cost, Spark preferred)
 
   test("CBO should prefer Comet for fast expressions (length)") {
-    withSQLConf(
-      CometConf.COMET_ENABLED.key -> "true",
-      CometConf.COMET_COST_BASED_OPTIMIZATION_ENABLED.key -> "true",
-      SQLConf.ADAPTIVE_EXECUTION_ENABLED.key -> "true",
-      CometConf.COMET_EXEC_ENABLED.key -> "true",
-      CometConf.COMET_EXEC_PROJECT_ENABLED.key -> "true") {
+    withCBOEnabled {
+      createSimpleTestData()
+      val query = "SELECT length(text1) as len1, length(text2) as len2 FROM test_data"
 
-      withTempView("test_data") {
-        // Create test data
-        import testImplicits._
-        val df = Seq(
-          ("hello world", "test string"),
-          ("comet rocks", "another test"),
-          ("fast execution", "performance")).toDF("text1", "text2")
-        df.createOrReplaceTempView("test_data")
-
-        // Query using length function (fast in Comet: 9.1x acceleration)
-        val query = "SELECT length(text1) as len1, length(text2) as len2 FROM test_data"
-        val result = sql(query)
-
-        // Execute the query to materialize the plan
-        result.collect()
-
-        // Check that Comet is used for the projection (due to low cost)
-        val executedPlan = stripAQEPlan(result.queryExecution.executedPlan)
-        val hasProjectExec = findProjectExec(executedPlan)
-
-        // With CBO enabled and fast expressions, we should see CometProjectExec
-        assert(hasProjectExec.isDefined, "Should have a project operator")
-        assert(
-          hasProjectExec.get.isInstanceOf[CometProjectExec],
-          s"Expected CometProjectExec for fast expression, got ${hasProjectExec.get.getClass.getSimpleName}")
-      }
+      executeAndCheckOperator(
+        query,
+        classOf[CometProjectExec],
+        "Expected CometProjectExec for fast expression")
     }
   }
 
   test("CBO should prefer Spark for slow expressions (trim)") {
-    withSQLConf(
-      CometConf.COMET_ENABLED.key -> "true",
-      CometConf.COMET_COST_BASED_OPTIMIZATION_ENABLED.key -> "true",
-      SQLConf.ADAPTIVE_EXECUTION_ENABLED.key -> "true",
-      CometConf.COMET_EXEC_ENABLED.key -> "true",
-      CometConf.COMET_EXEC_PROJECT_ENABLED.key -> "true") {
+    withCBOEnabled {
+      createPaddedTestData()
+      val query = "SELECT trim(text1) as trimmed1, trim(text2) as trimmed2 FROM test_data"
 
-      withTempView("test_data") {
-        // Create test data
-        import testImplicits._
-        val df = Seq(
-          ("  hello world  ", "  test string  "),
-          ("  comet rocks  ", "  another test  "),
-          ("  slow execution  ", "  performance  ")).toDF("text1", "text2")
-        df.createOrReplaceTempView("test_data")
-
-        // Query using trim function (slow in Comet: 0.4x acceleration)
-        val query = "SELECT trim(text1) as trimmed1, trim(text2) as trimmed2 FROM test_data"
-        val result = sql(query)
-
-        // Execute the query to materialize the plan
-        result.collect()
-
-        // Check that Spark is used for the projection (due to high cost)
-        val executedPlan = stripAQEPlan(result.queryExecution.executedPlan)
-        val hasProjectExec = findProjectExec(executedPlan)
-
-        // With CBO enabled and slow expressions, we should see Spark ProjectExec
-        assert(hasProjectExec.isDefined, "Should have a project operator")
-        assert(
-          hasProjectExec.get.isInstanceOf[ProjectExec],
-          s"Expected Spark ProjectExec for slow expression, got ${hasProjectExec.get.getClass.getSimpleName}")
-      }
+      executeAndCheckOperator(
+        query,
+        classOf[ProjectExec],
+        "Expected Spark ProjectExec for slow expression")
     }
   }
 
   test("Without CBO, Comet should be used regardless of expression cost") {
-    withSQLConf(
-      CometConf.COMET_ENABLED.key -> "true",
-      CometConf.COMET_COST_BASED_OPTIMIZATION_ENABLED.key -> "false", // CBO disabled
-      SQLConf.ADAPTIVE_EXECUTION_ENABLED.key -> "true",
-      CometConf.COMET_EXEC_ENABLED.key -> "true",
-      CometConf.COMET_EXEC_PROJECT_ENABLED.key -> "true") {
+    withCBODisabled {
+      createPaddedTestData()
+      val query = "SELECT trim(text1) as trimmed1 FROM test_data"
 
-      withTempView("test_data") {
-        import testImplicits._
-        val df = Seq(("  hello world  ", "  test string  ")).toDF("text1", "text2")
-        df.createOrReplaceTempView("test_data")
-
-        // Query using trim function (slow in Comet, but CBO is disabled)
-        val query = "SELECT trim(text1) as trimmed1 FROM test_data"
-        val result = sql(query)
-
-        result.collect()
-
-        val executedPlan = stripAQEPlan(result.queryExecution.executedPlan)
-        val hasProjectExec = findProjectExec(executedPlan)
-
-        // Without CBO, Comet should be used even for slow expressions
-        assert(hasProjectExec.isDefined, "Should have a project operator")
-        assert(
-          hasProjectExec.get.isInstanceOf[CometProjectExec],
-          s"Expected CometProjectExec when CBO disabled, got ${hasProjectExec.get.getClass.getSimpleName}")
-      }
+      executeAndCheckOperator(
+        query,
+        classOf[CometProjectExec],
+        "Expected CometProjectExec when CBO disabled")
     }
   }
 
   test("Mixed expressions should use appropriate operators per expression cost") {
+    withCBOEnabled {
+      createSimpleTestData()
+      // Query mixing fast (length: 9.1x) and slow (ascii: 0.6x) expressions
+      // Average acceleration: (9.1 + 0.6) / 2 = 4.85x -> cost = 0.206 (still prefer Comet)
+      val query = "SELECT length(text1) as fast_expr, ascii(text1) as slow_expr FROM test_data"
+
+      executeAndCheckOperator(
+        query,
+        classOf[CometProjectExec],
+        "Expected CometProjectExec for mixed expressions with positive average")
+    }
+  }
+
+  /** Helper method to run tests with CBO enabled */
+  private def withCBOEnabled(f: => Unit): Unit = {
     withSQLConf(
       CometConf.COMET_ENABLED.key -> "true",
       CometConf.COMET_COST_BASED_OPTIMIZATION_ENABLED.key -> "true",
       SQLConf.ADAPTIVE_EXECUTION_ENABLED.key -> "true",
       CometConf.COMET_EXEC_ENABLED.key -> "true",
       CometConf.COMET_EXEC_PROJECT_ENABLED.key -> "true") {
-
-      withTempView("test_data") {
-        import testImplicits._
-        val df = Seq(("hello world", "test")).toDF("text1", "text2")
-        df.createOrReplaceTempView("test_data")
-
-        // Query mixing fast (length: 9.1x) and slow (ascii: 0.6x) expressions
-        val query = "SELECT length(text1) as fast_expr, ascii(text1) as slow_expr FROM test_data"
-        val result = sql(query)
-
-        result.collect()
-
-        // The overall cost should be average: (9.1 + 0.6) / 2 = 4.85
-        // Cost = 1/4.85 = 0.206, which should still prefer Comet
-        val executedPlan = stripAQEPlan(result.queryExecution.executedPlan)
-        val hasProjectExec = findProjectExec(executedPlan)
-
-        assert(hasProjectExec.isDefined, "Should have a project operator")
-        // Mixed expressions with overall positive acceleration should use Comet
-        assert(
-          hasProjectExec.get.isInstanceOf[CometProjectExec],
-          s"Expected CometProjectExec for mixed expressions with positive average, got ${hasProjectExec.get.getClass.getSimpleName}")
-      }
+      f
     }
   }
 
-  /**
-   * Helper method to find ProjectExec or CometProjectExec in the plan tree
-   */
+  /** Helper method to run tests with CBO disabled */
+  private def withCBODisabled(f: => Unit): Unit = {
+    withSQLConf(
+      CometConf.COMET_ENABLED.key -> "true",
+      CometConf.COMET_COST_BASED_OPTIMIZATION_ENABLED.key -> "false",
+      SQLConf.ADAPTIVE_EXECUTION_ENABLED.key -> "true",
+      CometConf.COMET_EXEC_ENABLED.key -> "true",
+      CometConf.COMET_EXEC_PROJECT_ENABLED.key -> "true") {
+      f
+    }
+  }
+
+  /** Create simple test data for string operations */
+  private def createSimpleTestData(): Unit = {
+    withTempView("test_data") {
+      import testImplicits._
+      val df = Seq(
+        ("hello world", "test string"),
+        ("comet rocks", "another test"),
+        ("fast execution", "performance")).toDF("text1", "text2")
+      df.createOrReplaceTempView("test_data")
+    }
+  }
+
+  /** Create padded test data for trim operations */
+  private def createPaddedTestData(): Unit = {
+    withTempView("test_data") {
+      import testImplicits._
+      val df = Seq(
+        ("  hello world  ", "  test string  "),
+        ("  comet rocks  ", "  another test  "),
+        ("  slow execution  ", "  performance  ")).toDF("text1", "text2")
+      df.createOrReplaceTempView("test_data")
+    }
+  }
+
+  /** Execute query and check that the expected operator type is used */
+  private def executeAndCheckOperator(
+      query: String,
+      expectedClass: Class[_],
+      message: String): Unit = {
+    val result = sql(query)
+    result.collect() // Materialize the plan
+
+    val executedPlan = stripAQEPlan(result.queryExecution.executedPlan)
+    val hasProjectExec = findProjectExec(executedPlan)
+
+    assert(hasProjectExec.isDefined, "Should have a project operator")
+    assert(
+      expectedClass.isInstance(hasProjectExec.get),
+      s"$message, got ${hasProjectExec.get.getClass.getSimpleName}")
+  }
+
+  /** Helper method to find ProjectExec or CometProjectExec in the plan tree */
   private def findProjectExec(plan: SparkPlan): Option[SparkPlan] = {
     if (plan.isInstanceOf[ProjectExec] || plan.isInstanceOf[CometProjectExec]) {
       Some(plan)
