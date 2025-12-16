54e1a3312043f10f02ca478a987a8ea9247ab6bd
Andy Grove <agrove@apache.org>
2025-12-15 17:21:27 -0700

    remove nonsense test

--- spark/src/test/scala/org/apache/comet/CometCostModelSuite.scala
+++ spark/src/test/scala/org/apache/comet/CometCostModelSuite.scala
@@ -26,15 +26,6 @@ import org.apache.spark.sql.execution.SparkPlan
 
 class CometCostModelSuite extends CometTestBase {
 
-  // Fast expressions in Comet (high acceleration factor -> low cost -> preferred)
-  // Based on CometCostModel estimates:
-  // - length: 9.1x -> cost = 1/9.1 = 0.11 (very low cost, Comet preferred)
-  // - reverse: 6.9x -> cost = 1/6.9 = 0.145 (low cost, Comet preferred)
-
-  // Slow expressions in Comet (low acceleration factor -> high cost -> Spark preferred)
-  // - trim: 0.4x -> cost = 1/0.4 = 2.5 (high cost, Spark preferred)
-  // - ascii: 0.6x -> cost = 1/0.6 = 1.67 (high cost, Spark preferred)
-
   test("CBO should prefer Comet for fast expressions (length)") {
     withSQLConf(CometConf.COMET_COST_BASED_OPTIMIZATION_ENABLED.key -> "true") {
       withTempView("test_data") {
@@ -132,54 +123,5 @@ class CometCostModelSuite extends CometTestBase {
     }
 
     searchPlan(plan)
-  }
-
-  test("Direct cost model test - fast vs slow expressions") {
-    withSQLConf(CometConf.COMET_COST_BASED_OPTIMIZATION_ENABLED.key -> "true") {
-      withTempView("test_data") {
-        createSimpleTestData()
-
-        // Test the cost model directly on project operators
-        val lengthQuery = "SELECT length(text1) as len FROM test_data"
-        val trimQuery = "SELECT trim(text1) as trimmed FROM test_data"
-
-        val lengthPlan = sql(lengthQuery).queryExecution.optimizedPlan
-        val trimPlan = sql(trimQuery).queryExecution.optimizedPlan
-
-        // Find project nodes in the optimized plans
-        val lengthProject = findProjectInPlan(lengthPlan)
-        val trimProject = findProjectInPlan(trimPlan)
-
-        if (lengthProject.isDefined && trimProject.isDefined) {
-          val costModel = new org.apache.comet.cost.DefaultCometCostModel()
-
-          // Create mock Comet project operators (this would normally be done by the planner)
-          // For now, just verify the cost model has different estimates for different expressions
-          val lengthCost = 9.1 // Expected length acceleration
-          val trimCost = 0.4 // Expected trim acceleration
-
-          assert(
-            lengthCost > trimCost,
-            s"Length ($lengthCost) should be faster than trim ($trimCost)")
-
-          // Cost = 1/acceleration, so lower acceleration = higher cost
-          assert(
-            1.0 / trimCost > 1.0 / lengthCost,
-            s"Trim cost (${1.0 / trimCost}) should be higher than length cost (${1.0 / lengthCost})")
-        } else {
-          // Skip test if projections are optimized away
-          cancel("Projections were optimized away - using integration tests instead")
-        }
-      }
-    }
-  }
-
-  /** Helper to find Project nodes in a logical plan */
-  private def findProjectInPlan(plan: org.apache.spark.sql.catalyst.plans.logical.LogicalPlan)
-      : Option[org.apache.spark.sql.catalyst.plans.logical.Project] = {
-    plan match {
-      case p: org.apache.spark.sql.catalyst.plans.logical.Project => Some(p)
-      case _ => plan.children.flatMap(findProjectInPlan).headOption
-    }
   }
 }
