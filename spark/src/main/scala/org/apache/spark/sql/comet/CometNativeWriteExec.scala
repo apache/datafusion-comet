66c2df75a195013e8f8f25b64ca7871c8cb2636b
shekharrajak <shekharrajak@live.com>
2026-01-12 23:12:09 +0530

    Fix scalafix: remove unused taskAttemptId variable

--- spark/src/main/scala/org/apache/spark/sql/comet/CometNativeWriteExec.scala
+++ spark/src/main/scala/org/apache/spark/sql/comet/CometNativeWriteExec.scala
@@ -107,7 +107,6 @@ case class CometNativeWriteExec(nativeOp: Operator, child: SparkPlan, outputPath
     // Execute native write operation
     childRDD.mapPartitionsInternal { iter =>
       val partitionId = org.apache.spark.TaskContext.getPartitionId()
-      val taskAttemptId = org.apache.spark.TaskContext.get().taskAttemptId()
 
       val nativeMetrics = CometMetricNode.fromCometPlan(this)
       // Register before CometExecIterator so completion listeners run after iterator close
