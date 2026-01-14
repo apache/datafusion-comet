9b9e1fe6623db475d935bd68e45c87c51d90b5ed
comphead <comphead@ukr.net>
2026-01-13 21:03:55 -0800

    DataFusion 52 migration

--- native/core/src/execution/planner.rs
+++ native/core/src/execution/planner.rs
@@ -1009,8 +1009,8 @@ impl PhysicalPlanner {
                                 .as_any()
                                 .downcast_ref::<DataFusionLiteral>()
                                 .ok_or_else(|| {
-                                    GeneralError("Expected literal of default value.".to_string())
-                                })?;
+                                GeneralError("Expected literal of default value.".to_string())
+                            })?;
                             Ok(df_literal.value().clone())
                         })
                         .collect();
