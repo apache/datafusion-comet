480582c92568ca278a143b48798997057f20a2fb
Kazantsev Maksim <mn.kazantsev@gmail.com>
2026-01-22 21:14:32 +0400

    Fix fmt

--- spark/src/test/scala/org/apache/comet/CometMapExpressionSuite.scala
+++ spark/src/test/scala/org/apache/comet/CometMapExpressionSuite.scala
@@ -28,7 +28,7 @@ import org.apache.spark.sql.internal.SQLConf
 import org.apache.spark.sql.types.BinaryType
 
 import org.apache.comet.serde.CometCreateMap
-import org.apache.comet.testing.{DataGenOptions, FuzzDataGenerator, ParquetGenerator, SchemaGenOptions}
+import org.apache.comet.testing.{DataGenOptions, ParquetGenerator, SchemaGenOptions}
 
 class CometMapExpressionSuite extends CometTestBase {
 
