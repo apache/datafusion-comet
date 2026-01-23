e8b87e7fe9e7b9af5152857fe34e29806a93ea02
Parth Chandra <parthc@apache.org>
2026-01-23 08:12:42 -0800

    scalastyle

--- spark/src/main/scala/org/apache/comet/serde/operator/CometIcebergNativeScan.scala
+++ spark/src/main/scala/org/apache/comet/serde/operator/CometIcebergNativeScan.scala
@@ -22,7 +22,6 @@ package org.apache.comet.serde.operator
 import scala.collection.mutable
 import scala.jdk.CollectionConverters._
 
-import org.json4s._
 import org.json4s.JsonDSL._
 import org.json4s.jackson.JsonMethods._
 
