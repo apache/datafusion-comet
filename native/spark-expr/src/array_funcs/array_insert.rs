a3f7f66ca98e70232aab358afd3567c30b002d12
Oleks V <comphead@users.noreply.github.com>
2026-02-26 13:36:32 -0800

    deps: DataFusion 52.0.0 migration (SchemaAdapter changes, etc.) [iceberg] (#3536)

--- native/spark-expr/src/array_funcs/array_insert.rs
+++ native/spark-expr/src/array_funcs/array_insert.rs
@@ -96,8 +96,8 @@ impl PhysicalExpr for ArrayInsert {
         self
     }
 
-    fn fmt_sql(&self, _: &mut Formatter<'_>) -> std::fmt::Result {
-        unimplemented!()
+    fn fmt_sql(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        Display::fmt(self, f)
     }
 
     fn data_type(&self, input_schema: &Schema) -> DataFusionResult<DataType> {
--- native/spark-expr/src/math_funcs/internal/checkoverflow.rs
+++ native/spark-expr/src/math_funcs/internal/checkoverflow.rs
@@ -83,8 +83,8 @@ impl PhysicalExpr for CheckOverflow {
         self
     }
 
-    fn fmt_sql(&self, _: &mut Formatter<'_>) -> std::fmt::Result {
-        unimplemented!()
+    fn fmt_sql(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        Display::fmt(self, f)
     }
 
     fn data_type(&self, _: &Schema) -> datafusion::common::Result<DataType> {
