587ee97e97da0748385996e735c4bffa5f82ab70
0lai0 <yuchenlai87@gmail.com>
2026-03-14 23:03:57 +0800

    address comment split out the sha2 tests

--- spark/src/test/scala/org/apache/comet/CometExpressionSuite.scala
+++ spark/src/test/scala/org/apache/comet/CometExpressionSuite.scala
@@ -1885,15 +1885,19 @@ class CometExpressionSuite extends CometTestBase with AdaptiveSparkPlanHelper {
               |('Spark SQL  ', 10, 1.2), (NULL, NULL, NULL), ('', 0, 0.0), ('苹果手机', NULL, 3.999999)
               |, ('Spark SQL  ', 10, 1.2), (NULL, NULL, NULL), ('', 0, 0.0), ('苹果手机', NULL, 3.999999)
               |""".stripMargin)
-          // Use checkSparkAnswer instead of checkSparkAnswerAndOperator because sha2 falls back to Spark
-          checkSparkAnswer("""
+          checkSparkAnswerAndOperator("""
               |select
               |md5(col), md5(cast(a as string)), md5(cast(b as string)),
               |hash(col), hash(col, 1), hash(col, 0), hash(col, a, b), hash(b, a, col),
               |xxhash64(col), xxhash64(col, 1), xxhash64(col, 0), xxhash64(col, a, b), xxhash64(b, a, col),
               |crc32(col), crc32(cast(a as string)), crc32(cast(b as string)),
-              |sha2(col, 0), sha2(col, 256), sha2(col, 224), sha2(col, 384), sha2(col, 512), sha2(col, 128), sha2(col, -1),
               |sha1(col), sha1(cast(a as string)), sha1(cast(b as string))
+              |from test
+              |""".stripMargin)
+          // sha2 falls back to Spark, so only verify answer correctness
+          checkSparkAnswer("""
+              |select
+              |sha2(col, 0), sha2(col, 256), sha2(col, 224), sha2(col, 384), sha2(col, 512), sha2(col, 128), sha2(col, -1)
               |from test
               |""".stripMargin)
         }
@@ -1997,15 +2001,19 @@ class CometExpressionSuite extends CometTestBase with AdaptiveSparkPlanHelper {
             .insertInto(table)
           // with random generated data
           // disable cast(b as string) for now, as the cast from float to string may produce incompatible result
-          // Use checkSparkAnswer instead of checkSparkAnswerAndOperator because sha2 falls back to Spark
-          checkSparkAnswer("""
+          checkSparkAnswerAndOperator("""
               |select
               |md5(col), md5(cast(a as string)), --md5(cast(b as string)),
               |hash(col), hash(col, 1), hash(col, 0), hash(col, a, b), hash(b, a, col),
               |xxhash64(col), xxhash64(col, 1), xxhash64(col, 0), xxhash64(col, a, b), xxhash64(b, a, col),
               |crc32(col), crc32(cast(a as string)),
-              |sha2(col, 0), sha2(col, 256), sha2(col, 224), sha2(col, 384), sha2(col, 512), sha2(col, 128), sha2(col, -1),
               |sha1(col), sha1(cast(a as string))
+              |from test
+              |""".stripMargin)
+          // sha2 falls back to Spark, so only verify answer correctness
+          checkSparkAnswer("""
+              |select
+              |sha2(col, 0), sha2(col, 256), sha2(col, 224), sha2(col, 384), sha2(col, 512), sha2(col, 128), sha2(col, -1)
               |from test
               |""".stripMargin)
         }
