fad07b98f798ae38fbf2ea0ed009cb8dd012c20d
Andy Grove <agrove@apache.org>
2026-03-16 12:58:10 -0600

    fix: explain NaN incompatibility in ArrayPosition getSupportLevel

--- spark/src/main/scala/org/apache/comet/serde/arrays.scala
+++ spark/src/main/scala/org/apache/comet/serde/arrays.scala
@@ -664,7 +664,10 @@ object CometSize extends CometExpressionSerde[Size] {
 
 object CometArrayPosition extends CometExpressionSerde[ArrayPosition] with ArraysBase {
 
-  override def getSupportLevel(expr: ArrayPosition): SupportLevel = Incompatible(None)
+  override def getSupportLevel(expr: ArrayPosition): SupportLevel =
+    Incompatible(Some(
+      "element comparison uses IEEE 754 equality where NaN != NaN, " +
+        "but Spark treats NaN as equal to NaN"))
 
   override def convert(
       expr: ArrayPosition,
