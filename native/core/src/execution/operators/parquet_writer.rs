15a91f975145763e71616233463361b0d19d4d89
Matt Butrovich <mbutrovich@gmail.com>
2026-04-08 17:37:36 -0400

    Start testing what will be DataFusion 54.0.

--- native/core/src/execution/operators/parquet_writer.rs
+++ native/core/src/execution/operators/parquet_writer.rs
@@ -37,6 +37,7 @@ use crate::parquet::parquet_support::{create_hdfs_operator, prepare_object_store
 use arrow::datatypes::{Schema, SchemaRef};
 use arrow::record_batch::RecordBatch;
 use async_trait::async_trait;
+use datafusion::common::tree_node::TreeNodeRecursion;
 use datafusion::{
     error::{DataFusionError, Result},
     execution::context::TaskContext,
@@ -45,8 +46,8 @@ use datafusion::{
         execution_plan::{Boundedness, EmissionType},
         metrics::{ExecutionPlanMetricsSet, MetricsSet},
         stream::RecordBatchStreamAdapter,
-        DisplayAs, DisplayFormatType, ExecutionPlan, ExecutionPlanProperties, PlanProperties,
-        SendableRecordBatchStream,
+        DisplayAs, DisplayFormatType, ExecutionPlan, ExecutionPlanProperties, PhysicalExpr,
+        PlanProperties, SendableRecordBatchStream,
     },
 };
 use futures::TryStreamExt;
@@ -405,6 +406,13 @@ impl DisplayAs for ParquetWriterExec {
 impl ExecutionPlan for ParquetWriterExec {
     fn name(&self) -> &str {
         "ParquetWriterExec"
+    }
+
+    fn apply_expressions(
+        &self,
+        _f: &mut dyn FnMut(&dyn PhysicalExpr) -> Result<TreeNodeRecursion>,
+    ) -> Result<TreeNodeRecursion> {
+        Ok(TreeNodeRecursion::Continue)
     }
 
     fn metrics(&self) -> Option<MetricsSet> {
