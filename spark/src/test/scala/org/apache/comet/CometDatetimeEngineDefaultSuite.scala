c0818118956d701592d1995de8bf626d7e75dc76
Andy Grove <agrove@apache.org>
2026-05-14 09:04:52 -0600

    test(datetime): assert default engine remains rust [skip ci]

---
+++ spark/src/test/scala/org/apache/comet/CometDatetimeEngineDefaultSuite.scala
@@ -0,0 +1,35 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.comet
+
+import org.apache.spark.sql.CometTestBase
+import org.apache.spark.sql.execution.adaptive.AdaptiveSparkPlanHelper
+
+class CometDatetimeEngineDefaultSuite extends CometTestBase with AdaptiveSparkPlanHelper {
+
+  test("default engine is rust: TimestampType hour still runs natively") {
+    assert(CometConf.COMET_DATETIME_ENGINE.get() == CometConf.DATETIME_ENGINE_RUST)
+    withTable("t") {
+      sql("CREATE TABLE t (ts TIMESTAMP) USING parquet")
+      sql("INSERT INTO t VALUES TIMESTAMP'2024-06-15 12:34:56 UTC'")
+      checkSparkAnswerAndOperator("SELECT hour(ts), minute(ts), second(ts) FROM t")
+    }
+  }
+}
