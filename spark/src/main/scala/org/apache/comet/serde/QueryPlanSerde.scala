2826f15d7cb1d5abfc940e382141a66c5f73ed41
Andy Grove <agrove@apache.org>
2026-05-24 09:33:30 -0600

    feat(datetime): route UnixMillis through codegen dispatcher [skip ci]

--- spark/src/main/scala/org/apache/comet/serde/QueryPlanSerde.scala
+++ spark/src/main/scala/org/apache/comet/serde/QueryPlanSerde.scala
@@ -245,6 +245,7 @@ object QueryPlanSerde extends Logging with CometExprShim with CometTypeShim {
       classOf[SecondsToTimestamp] -> CometSecondsToTimestamp,
       classOf[TruncDate] -> CometTruncDate,
       classOf[TruncTimestamp] -> CometTruncTimestamp,
+      classOf[UnixMillis] -> CometUnixMillis,
       classOf[UnixSeconds] -> CometUnixSeconds,
       classOf[UnixTimestamp] -> CometUnixTimestamp,
       classOf[Year] -> CometYear,
