71693994b417be5bff9e077706da0005e14711cf
subhodeep <subhodeeppal64@gmail.com>
2026-05-25 12:10:20 +0000

    (g) => fix inside the doGenCode

--- spark/src/main/scala/org/apache/comet/expressions/GeoExpressions.scala
+++ spark/src/main/scala/org/apache/comet/expressions/GeoExpressions.scala
@@ -165,7 +165,7 @@ case class StArea(child: Expression) extends UnaryExpression with NullIntolerant
     defineCodeGen(
       ctx,
       ev,
-      { g => s"org.apache.comet.expressions.CometGeoFallback$$.MODULE$$.area($g.toString())" })
+      g => s"org.apache.comet.expressions.CometGeoFallback$$.MODULE$$.area($g.toString())")
   override protected def withNewChildInternal(newChild: Expression): Expression =
     copy(child = newChild)
 }
@@ -178,9 +178,9 @@ case class StCentroid(child: Expression) extends UnaryExpression with NullIntole
     defineCodeGen(
       ctx,
       ev,
-      { g =>
+      g =>
         s"org.apache.spark.unsafe.types.UTF8String.fromString(" +
-          s"org.apache.comet.expressions.CometGeoFallback$$.MODULE$$.centroid($g.toString()))" })
+          s"org.apache.comet.expressions.CometGeoFallback$$.MODULE$$.centroid($g.toString()))")
   override protected def withNewChildInternal(newChild: Expression): Expression =
     copy(child = newChild)
 }
@@ -192,7 +192,7 @@ case class StLength(child: Expression) extends UnaryExpression with NullIntolera
     defineCodeGen(
       ctx,
       ev,
-      { g => s"org.apache.comet.expressions.CometGeoFallback$$.MODULE$$.length($g.toString())" })
+      g => s"org.apache.comet.expressions.CometGeoFallback$$.MODULE$$.length($g.toString())")
   override protected def withNewChildInternal(newChild: Expression): Expression =
     copy(child = newChild)
 }
@@ -204,7 +204,7 @@ case class StIsEmpty(child: Expression) extends UnaryExpression with NullIntoler
     defineCodeGen(
       ctx,
       ev,
-      { g => s"org.apache.comet.expressions.CometGeoFallback$$.MODULE$$.isEmpty($g.toString())" })
+      g => s"org.apache.comet.expressions.CometGeoFallback$$.MODULE$$.isEmpty($g.toString())")
   override protected def withNewChildInternal(newChild: Expression): Expression =
     copy(child = newChild)
 }
@@ -217,10 +217,10 @@ case class StGeometryType(child: Expression) extends UnaryExpression with NullIn
     defineCodeGen(
       ctx,
       ev,
-      { g =>
+      g =>
         s"org.apache.spark.unsafe.types.UTF8String.fromString(" +
           s"org.apache.comet.expressions.CometGeoFallback$$.MODULE$$" +
-          s".geometryType($g.toString()))" })
+          s".geometryType($g.toString()))")
   override protected def withNewChildInternal(newChild: Expression): Expression =
     copy(child = newChild)
 }
@@ -232,7 +232,7 @@ case class StNumPoints(child: Expression) extends UnaryExpression with NullIntol
     defineCodeGen(
       ctx,
       ev,
-      { g => s"org.apache.comet.expressions.CometGeoFallback$$.MODULE$$.numPoints($g.toString())" })
+      g => s"org.apache.comet.expressions.CometGeoFallback$$.MODULE$$.numPoints($g.toString())")
   override protected def withNewChildInternal(newChild: Expression): Expression =
     copy(child = newChild)
 }
@@ -244,7 +244,7 @@ case class StX(child: Expression) extends UnaryExpression with NullIntolerant {
     defineCodeGen(
       ctx,
       ev,
-      { g => s"org.apache.comet.expressions.CometGeoFallback$$.MODULE$$.stX($g.toString())" })
+      g => s"org.apache.comet.expressions.CometGeoFallback$$.MODULE$$.stX($g.toString())")
   override protected def withNewChildInternal(newChild: Expression): Expression =
     copy(child = newChild)
 }
@@ -256,7 +256,7 @@ case class StY(child: Expression) extends UnaryExpression with NullIntolerant {
     defineCodeGen(
       ctx,
       ev,
-      { g => s"org.apache.comet.expressions.CometGeoFallback$$.MODULE$$.stY($g.toString())" })
+      g => s"org.apache.comet.expressions.CometGeoFallback$$.MODULE$$.stY($g.toString())")
   override protected def withNewChildInternal(newChild: Expression): Expression =
     copy(child = newChild)
 }
@@ -269,9 +269,9 @@ case class StEnvelope(child: Expression) extends UnaryExpression with NullIntole
     defineCodeGen(
       ctx,
       ev,
-      { g =>
+      g =>
         s"org.apache.spark.unsafe.types.UTF8String.fromString(" +
-          s"org.apache.comet.expressions.CometGeoFallback$$.MODULE$$.envelope($g.toString()))" })
+          s"org.apache.comet.expressions.CometGeoFallback$$.MODULE$$.envelope($g.toString()))")
   override protected def withNewChildInternal(newChild: Expression): Expression =
     copy(child = newChild)
 }
@@ -284,10 +284,10 @@ case class StConvexHull(child: Expression) extends UnaryExpression with NullInto
     defineCodeGen(
       ctx,
       ev,
-      { g =>
+      g =>
         s"org.apache.spark.unsafe.types.UTF8String.fromString(" +
           s"org.apache.comet.expressions.CometGeoFallback$$.MODULE$$" +
-          s".convexHull($g.toString()))" })
+          s".convexHull($g.toString()))")
   override protected def withNewChildInternal(newChild: Expression): Expression =
     copy(child = newChild)
 }
@@ -342,9 +342,9 @@ case class StGeomFromWkt(child: Expression) extends UnaryExpression with NullInt
     defineCodeGen(
       ctx,
       ev,
-      { g =>
+      g =>
         s"org.apache.spark.unsafe.types.UTF8String.fromString(" +
-          s"org.apache.comet.expressions.CometGeoFallback$$.MODULE$$.geomFromWkt($g.toString()))" })
+          s"org.apache.comet.expressions.CometGeoFallback$$.MODULE$$.geomFromWkt($g.toString()))")
   override protected def withNewChildInternal(newChild: Expression): Expression =
     copy(child = newChild)
 }
@@ -357,10 +357,10 @@ case class StGeomFromGeoJson(child: Expression) extends UnaryExpression with Nul
     defineCodeGen(
       ctx,
       ev,
-      { g =>
+      g =>
         s"org.apache.spark.unsafe.types.UTF8String.fromString(" +
           s"org.apache.comet.expressions.CometGeoFallback$$.MODULE$$" +
-          s".geomFromGeoJson($g.toString()))" })
+          s".geomFromGeoJson($g.toString()))")
   override protected def withNewChildInternal(newChild: Expression): Expression =
     copy(child = newChild)
 }
@@ -424,9 +424,9 @@ case class StAsText(child: Expression) extends UnaryExpression with NullIntolera
     defineCodeGen(
       ctx,
       ev,
-      { g =>
+      g =>
         s"org.apache.spark.unsafe.types.UTF8String.fromString(" +
-          s"org.apache.comet.expressions.CometGeoFallback$$.MODULE$$.asText($g.toString()))" })
+          s"org.apache.comet.expressions.CometGeoFallback$$.MODULE$$.asText($g.toString()))")
   override protected def withNewChildInternal(newChild: Expression): Expression =
     copy(child = newChild)
 }
@@ -439,9 +439,9 @@ case class StAsGeoJson(child: Expression) extends UnaryExpression with NullIntol
     defineCodeGen(
       ctx,
       ev,
-      { g =>
+      g =>
         s"org.apache.spark.unsafe.types.UTF8String.fromString(" +
-          s"org.apache.comet.expressions.CometGeoFallback$$.MODULE$$.asGeoJson($g.toString()))" })
+          s"org.apache.comet.expressions.CometGeoFallback$$.MODULE$$.asGeoJson($g.toString()))")
   override protected def withNewChildInternal(newChild: Expression): Expression =
     copy(child = newChild)
 }
@@ -601,8 +601,8 @@ case class StPerimeter(child: Expression) extends UnaryExpression with NullIntol
     defineCodeGen(
       ctx,
       ev,
-      { g =>
-        s"org.apache.comet.expressions.CometGeoFallback$$.MODULE$$.perimeter($g.toString())" })
+      g =>
+        s"org.apache.comet.expressions.CometGeoFallback$$.MODULE$$.perimeter($g.toString())")
   override protected def withNewChildInternal(newChild: Expression): Expression =
     copy(child = newChild)
 }
@@ -655,10 +655,10 @@ case class StFlipCoordinates(child: Expression) extends UnaryExpression with Nul
     defineCodeGen(
       ctx,
       ev,
-      { g =>
+      g =>
         s"org.apache.spark.unsafe.types.UTF8String.fromString(" +
           s"org.apache.comet.expressions.CometGeoFallback$$.MODULE$$" +
-          s".flipCoordinates($g.toString()))" })
+          s".flipCoordinates($g.toString()))")
   override protected def withNewChildInternal(newChild: Expression): Expression =
     copy(child = newChild)
 }
@@ -671,9 +671,9 @@ case class StBoundary(child: Expression) extends UnaryExpression with NullIntole
     defineCodeGen(
       ctx,
       ev,
-      { g =>
+      g =>
         s"org.apache.spark.unsafe.types.UTF8String.fromString(" +
-          s"org.apache.comet.expressions.CometGeoFallback$$.MODULE$$.boundary($g.toString()))" })
+          s"org.apache.comet.expressions.CometGeoFallback$$.MODULE$$.boundary($g.toString()))")
   override protected def withNewChildInternal(newChild: Expression): Expression =
     copy(child = newChild)
 }
@@ -735,131 +735,131 @@ object GeoExpressions {
       builder)
 
   val stContainsInfo: FunctionDescription =
-    desc("st_contains", classOf[StContains], (args) => StContains(args(0), args(1)))
+    desc("st_contains", classOf[StContains], args => StContains(args(0), args(1)))
 
   val stIntersectsInfo: FunctionDescription =
-    desc("st_intersects", classOf[StIntersects], (args) => StIntersects(args(0), args(1)))
+    desc("st_intersects", classOf[StIntersects], args => StIntersects(args(0), args(1)))
 
   val stWithinInfo: FunctionDescription =
-    desc("st_within", classOf[StWithin], (args) => StWithin(args(0), args(1)))
+    desc("st_within", classOf[StWithin], args => StWithin(args(0), args(1)))
 
   val stDistanceInfo: FunctionDescription =
-    desc("st_distance", classOf[StDistance], (args) => StDistance(args(0), args(1)))
+    desc("st_distance", classOf[StDistance], args => StDistance(args(0), args(1)))
 
   val stAreaInfo: FunctionDescription =
-    desc("st_area", classOf[StArea], (args) => StArea(args(0)))
+    desc("st_area", classOf[StArea], args => StArea(args(0)))
 
   val stCentroidInfo: FunctionDescription =
-    desc("st_centroid", classOf[StCentroid], (args) => StCentroid(args(0)))
+    desc("st_centroid", classOf[StCentroid], args => StCentroid(args(0)))
 
   val stLengthInfo: FunctionDescription =
-    desc("st_length", classOf[StLength], (args) => StLength(args(0)))
+    desc("st_length", classOf[StLength], args => StLength(args(0)))
 
   val stIsEmptyInfo: FunctionDescription =
-    desc("st_isempty", classOf[StIsEmpty], (args) => StIsEmpty(args(0)))
+    desc("st_isempty", classOf[StIsEmpty], args => StIsEmpty(args(0)))
 
   val stGeometryTypeInfo: FunctionDescription =
-    desc("st_geometrytype", classOf[StGeometryType], (args) => StGeometryType(args(0)))
+    desc("st_geometrytype", classOf[StGeometryType], args => StGeometryType(args(0)))
 
   val stNumPointsInfo: FunctionDescription =
-    desc("st_numpoints", classOf[StNumPoints], (args) => StNumPoints(args(0)))
+    desc("st_numpoints", classOf[StNumPoints], args => StNumPoints(args(0)))
 
   val stXInfo: FunctionDescription =
-    desc("st_x", classOf[StX], (args) => StX(args(0)))
+    desc("st_x", classOf[StX], args => StX(args(0)))
 
   val stYInfo: FunctionDescription =
-    desc("st_y", classOf[StY], (args) => StY(args(0)))
+    desc("st_y", classOf[StY], args => StY(args(0)))
 
   val stEnvelopeInfo: FunctionDescription =
-    desc("st_envelope", classOf[StEnvelope], (args) => StEnvelope(args(0)))
+    desc("st_envelope", classOf[StEnvelope], args => StEnvelope(args(0)))
 
   val stConvexHullInfo: FunctionDescription =
-    desc("st_convexhull", classOf[StConvexHull], (args) => StConvexHull(args(0)))
+    desc("st_convexhull", classOf[StConvexHull], args => StConvexHull(args(0)))
 
   val stSimplifyInfo: FunctionDescription =
-    desc("st_simplify", classOf[StSimplify], (args) => StSimplify(args(0), args(1)))
+    desc("st_simplify", classOf[StSimplify], args => StSimplify(args(0), args(1)))
 
   val stBufferInfo: FunctionDescription =
-    desc("st_buffer", classOf[StBuffer], (args) => StBuffer(args(0), args(1)))
+    desc("st_buffer", classOf[StBuffer], args => StBuffer(args(0), args(1)))
 
   val stUnionInfo: FunctionDescription =
-    desc("st_union", classOf[StUnion], (args) => StUnion(args(0), args(1)))
+    desc("st_union", classOf[StUnion], args => StUnion(args(0), args(1)))
 
   val stIntersectionInfo: FunctionDescription =
-    desc("st_intersection", classOf[StIntersection], (args) => StIntersection(args(0), args(1)))
+    desc("st_intersection", classOf[StIntersection], args => StIntersection(args(0), args(1)))
 
   val stGeomFromWktInfo: FunctionDescription =
-    desc("st_geomfromwkt", classOf[StGeomFromWkt], (args) => StGeomFromWkt(args(0)))
+    desc("st_geomfromwkt", classOf[StGeomFromWkt], args => StGeomFromWkt(args(0)))
 
   val stGeomFromGeoJsonInfo: FunctionDescription =
-    desc("st_geomfromgeojson", classOf[StGeomFromGeoJson], (args) => StGeomFromGeoJson(args(0)))
+    desc("st_geomfromgeojson", classOf[StGeomFromGeoJson], args => StGeomFromGeoJson(args(0)))
 
   val stPointInfo: FunctionDescription =
-    desc("st_point", classOf[StPoint], (args) => StPoint(args(0), args(1)))
+    desc("st_point", classOf[StPoint], args => StPoint(args(0), args(1)))
 
   val stMakeEnvelopeInfo: FunctionDescription =
     desc(
       "st_makeenvelope",
       classOf[StMakeEnvelope],
-      (args) => StMakeEnvelope(args(0), args(1), args(2), args(3)))
+      args => StMakeEnvelope(args(0), args(1), args(2), args(3)))
 
   val stMakeLineInfo: FunctionDescription =
-    desc("st_makeline", classOf[StMakeLine], (args) => StMakeLine(args(0), args(1)))
+    desc("st_makeline", classOf[StMakeLine], args => StMakeLine(args(0), args(1)))
 
   val stAsTextInfo: FunctionDescription =
-    desc("st_astext", classOf[StAsText], (args) => StAsText(args(0)))
+    desc("st_astext", classOf[StAsText], args => StAsText(args(0)))
 
   val stAsGeoJsonInfo: FunctionDescription =
-    desc("st_asgeojson", classOf[StAsGeoJson], (args) => StAsGeoJson(args(0)))
+    desc("st_asgeojson", classOf[StAsGeoJson], args => StAsGeoJson(args(0)))
 
   val stCoversInfo: FunctionDescription =
-    desc("st_covers", classOf[StCovers], (args) => StCovers(args(0), args(1)))
+    desc("st_covers", classOf[StCovers], args => StCovers(args(0), args(1)))
 
   val stCoveredByInfo: FunctionDescription =
-    desc("st_coveredby", classOf[StCoveredBy], (args) => StCoveredBy(args(0), args(1)))
+    desc("st_coveredby", classOf[StCoveredBy], args => StCoveredBy(args(0), args(1)))
 
   val stEqualsInfo: FunctionDescription =
-    desc("st_equals", classOf[StEquals], (args) => StEquals(args(0), args(1)))
+    desc("st_equals", classOf[StEquals], args => StEquals(args(0), args(1)))
 
   val stTouchesInfo: FunctionDescription =
-    desc("st_touches", classOf[StTouches], (args) => StTouches(args(0), args(1)))
+    desc("st_touches", classOf[StTouches], args => StTouches(args(0), args(1)))
 
   val stCrossesInfo: FunctionDescription =
-    desc("st_crosses", classOf[StCrosses], (args) => StCrosses(args(0), args(1)))
+    desc("st_crosses", classOf[StCrosses], args => StCrosses(args(0), args(1)))
 
   val stDisjointInfo: FunctionDescription =
-    desc("st_disjoint", classOf[StDisjoint], (args) => StDisjoint(args(0), args(1)))
+    desc("st_disjoint", classOf[StDisjoint], args => StDisjoint(args(0), args(1)))
 
   val stOverlapsInfo: FunctionDescription =
-    desc("st_overlaps", classOf[StOverlaps], (args) => StOverlaps(args(0), args(1)))
+    desc("st_overlaps", classOf[StOverlaps], args => StOverlaps(args(0), args(1)))
 
   val stDistanceSphereInfo: FunctionDescription =
-    desc("st_distancesphere", classOf[StDistanceSphere], (args) => StDistanceSphere(args(0), args(1)))
+    desc("st_distancesphere", classOf[StDistanceSphere], args => StDistanceSphere(args(0), args(1)))
 
   val stPerimeterInfo: FunctionDescription =
-    desc("st_perimeter", classOf[StPerimeter], (args) => StPerimeter(args(0)))
+    desc("st_perimeter", classOf[StPerimeter], args => StPerimeter(args(0)))
 
   val stHausdorffDistanceInfo: FunctionDescription =
     desc(
       "st_hausdorffdistance",
       classOf[StHausdorffDistance],
-      (args) => StHausdorffDistance(args(0), args(1)))
+      args => StHausdorffDistance(args(0), args(1)))
 
   val stSimplifyPreserveTopologyInfo: FunctionDescription =
     desc(
       "st_simplifypreservetopology",
       classOf[StSimplifyPreserveTopology],
-      (args) => StSimplifyPreserveTopology(args(0), args(1)))
+      args => StSimplifyPreserveTopology(args(0), args(1)))
 
   val stFlipCoordinatesInfo: FunctionDescription =
-    desc("st_flipcoordinates", classOf[StFlipCoordinates], (args) => StFlipCoordinates(args(0)))
+    desc("st_flipcoordinates", classOf[StFlipCoordinates], args => StFlipCoordinates(args(0)))
 
   val stBoundaryInfo: FunctionDescription =
-    desc("st_boundary", classOf[StBoundary], (args) => StBoundary(args(0)))
+    desc("st_boundary", classOf[StBoundary], args => StBoundary(args(0)))
 
   val stDifferenceInfo: FunctionDescription =
-    desc("st_difference", classOf[StDifference], (args) => StDifference(args(0), args(1)))
+    desc("st_difference", classOf[StDifference], args => StDifference(args(0), args(1)))
 
   val stSymDifferenceInfo: FunctionDescription =
-    desc("st_symdifference", classOf[StSymDifference], (args) => StSymDifference(args(0), args(1)))
+    desc("st_symdifference", classOf[StSymDifference], args => StSymDifference(args(0), args(1)))
 }
