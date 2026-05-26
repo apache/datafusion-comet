33ab20afb73dbd7462ac808d229f3fdf7d6ca9ad
Andy Grove <agrove@apache.org>
2026-05-26 10:53:11 -0600

    chore: remove dead vector classes left over from native_iceberg_compat removal (#4416)

--- spark/src/main/java/org/apache/comet/vector/CometDecodedVector.java
+++ spark/src/main/java/org/apache/comet/vector/CometDecodedVector.java
@@ -19,13 +19,19 @@
 
 package org.apache.comet.vector;
 
-import org.apache.arrow.vector.BaseVariableWidthVector;
 import org.apache.arrow.vector.ValueVector;
 import org.apache.arrow.vector.types.pojo.Field;
 import org.apache.spark.sql.comet.util.Utils;
 import org.apache.spark.unsafe.Platform;
 
-/** A Comet vector whose elements are already decoded (i.e., materialized). */
+/**
+ * A Comet vector backed by a single Arrow {@link ValueVector} whose values are already decoded (vs.
+ * {@link CometDictionaryVector}, which keeps indices and dictionary values in separate vectors and
+ * decodes on access).
+ *
+ * <p>Caches the most recently read validity byte to amortize null checks during the common
+ * sequential row-access pattern.
+ */
 public abstract class CometDecodedVector extends CometVector {
   /**
    * The vector that stores all the values. For dictionary-backed vector, this is the vector of
@@ -33,9 +39,9 @@ public abstract class CometDecodedVector extends CometVector {
    */
   protected final ValueVector valueVector;
 
-  private boolean hasNull;
-  private int numNulls;
-  private int numValues;
+  private final boolean hasNull;
+  private final int numNulls;
+  private final int numValues;
   private int validityByteCacheIndex = -1;
   private byte validityByteCache;
   protected boolean isUuid;
@@ -56,31 +62,6 @@ public abstract class CometDecodedVector extends CometVector {
   @Override
   public ValueVector getValueVector() {
     return valueVector;
-  }
-
-  @Override
-  public void setNumNulls(int numNulls) {
-    // We don't need to update null count in 'valueVector' since 'ValueVector.getNullCount' will
-    // re-compute the null count from validity buffer.
-    this.numNulls = numNulls;
-    this.hasNull = numNulls != 0;
-    this.validityByteCacheIndex = -1;
-  }
-
-  @Override
-  public void setNumValues(int numValues) {
-    this.numValues = numValues;
-    if (valueVector instanceof BaseVariableWidthVector) {
-      BaseVariableWidthVector bv = (BaseVariableWidthVector) valueVector;
-      // In case `lastSet` is smaller than `numValues`, `setValueCount` will set all the offsets
-      // within `[lastSet + 1, numValues)` to be empty, which is incorrect in our case.
-      //
-      // For instance, this can happen if one first call `setNumValues` with input 100, and then
-      // again `setNumValues` with 200. The first call will set `lastSet` to 99, while the second
-      // call will set all strings between indices `[100, 200)` to be empty.
-      bv.setLastSet(numValues);
-    }
-    valueVector.setValueCount(numValues);
   }
 
   public int numValues() {
