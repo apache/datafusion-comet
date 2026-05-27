68381a01b102236077a64e1f9465facf3ad0d930
Matt Butrovich <mbutrovich@gmail.com>
2026-05-27 17:40:50 -0400

    Tighten tests.

--- spark/src/test/scala/org/apache/comet/exec/CometExecSuite.scala
+++ spark/src/test/scala/org/apache/comet/exec/CometExecSuite.scala
@@ -3928,15 +3928,15 @@ class CometExecSuite extends CometTestBase {
   test("CometLocalTableScanExec handles NullType column") {
     withSQLConf(CometConf.COMET_EXEC_LOCAL_TABLE_SCAN_ENABLED.key -> "true") {
       val df = spark.sql("SELECT * FROM VALUES ('a', null), ('b', null) AS t(x, y)")
-      checkSparkAnswer(df)
+      checkSparkAnswerAndOperator(df)
     }
   }
 
   test("CometLocalTableScanExec handles NullType nested in struct/array/map") {
     withSQLConf(CometConf.COMET_EXEC_LOCAL_TABLE_SCAN_ENABLED.key -> "true") {
-      checkSparkAnswer(
+      checkSparkAnswerAndOperator(
         spark.sql("SELECT named_struct('a', 1, 'b', null) AS s, array(null, null) AS a, " +
-          "map('k', null) AS m"))
+          "map('k', null) AS m FROM VALUES (1), (2) AS t(id)"))
     }
   }
 
