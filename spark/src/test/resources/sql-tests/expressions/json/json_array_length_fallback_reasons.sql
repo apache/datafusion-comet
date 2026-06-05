82d674e3362a0670dc22e6feeafd81a58e12e5e6
Kazantsev Maksim <kazantsev.maksim.n@gmail.com>
2026-06-05 05:02:22 +0400

    feat: support Spark expression json_array_length (#4365)

---
+++ spark/src/test/resources/sql-tests/expressions/json/json_array_length_fallback_reasons.sql
@@ -0,0 +1,21 @@
+-- Licensed to the Apache Software Foundation (ASF) under one
+-- or more contributor license agreements.  See the NOTICE file
+-- distributed with this work for additional information
+-- regarding copyright ownership.  The ASF licenses this file
+-- to you under the Apache License, Version 2.0 (the
+-- "License"); you may not use this file except in compliance
+-- with the License.  You may obtain a copy of the License at
+--
+--   http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing,
+-- software distributed under the License is distributed on an
+-- "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+-- KIND, either express or implied.  See the License for the
+-- specific language governing permissions and limitations
+-- under the License.
+
+-- Config: spark.comet.expression.LengthOfJsonArray.allowIncompatible=false
+
+query expect_fallback(Spark's lenient JSON parser allows single quotes, unescaped controls, and trailing content, while Comet's serde_json requires strict JSON.)
+SELECT json_array_length("[{'key':'value'}]")
