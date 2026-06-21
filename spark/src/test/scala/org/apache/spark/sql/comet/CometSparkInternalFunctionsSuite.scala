ebbaf7268aca562a70b77366e1266cc6b28568c5
Kazantsev Maksim <mn.kazantsev@gmail.com>
2026-06-21 17:08:51 +0400

    add tests

--- spark/src/test/scala/org/apache/spark/sql/comet/CometSparkInternalFunctionsSuite.scala
+++ spark/src/test/scala/org/apache/spark/sql/comet/CometSparkInternalFunctionsSuite.scala
@@ -19,10 +19,7 @@
 
 package org.apache.spark.sql.comet
 
-import scala.util.Using
-
-import org.apache.hadoop.fs.{FileSystem, Path}
-import org.apache.spark.sql.{CometTestBase, Row}
+import org.apache.spark.sql.CometTestBase
 import org.apache.spark.sql.catalyst.expressions.Empty2Null
 import org.apache.spark.sql.classic.ExpressionUtils
 import org.apache.spark.sql.functions._
@@ -30,7 +27,6 @@ import org.apache.spark.sql.functions._
 import org.apache.comet.CometSparkSessionExtensions.isSpark40Plus
 
 class CometSparkInternalFunctionsSuite extends CometTestBase {
-  import testImplicits._
 
   test("empty2null is offloaded to Comet") {
     assume(isSpark40Plus)
@@ -38,27 +34,6 @@ class CometSparkInternalFunctionsSuite extends CometTestBase {
       val df = sql("select _1 from tbl")
         .select(ExpressionUtils.column(Empty2Null(ExpressionUtils.expression(col("_1")))).as("p"))
       checkSparkAnswerAndOperator(df)
-    }
-  }
-
-  test("partitioned write with empty string partition value") {
-    withTempPath { path =>
-      Seq(("", 1), ("a", 2))
-        .toDF("part", "value")
-        .write
-        .partitionBy("part")
-        .parquet(path.toString)
-      Using(FileSystem.get(spark.sparkContext.hadoopConfiguration)) { fs =>
-        val partitions = fs
-          .listStatus(new Path(path.toString))
-          .filter(_.isDirectory)
-          .map(_.getPath.getName)
-          .sorted
-        assert(partitions.contains("part=a"))
-        assert(!partitions.contains("part="))
-        assert(partitions.count(_.startsWith("part=__HIVE_DEFAULT_PARTITION__")) == 1)
-      }
-      checkAnswer(spark.read.parquet(path.toString), Row(1, null) :: Row(2, "a") :: Nil)
     }
   }
 }
