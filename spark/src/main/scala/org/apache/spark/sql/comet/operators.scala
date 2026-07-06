268319343850cf8914c759c63358de01c4f02b7c
comphead <comphead@ukr.net>
2026-07-06 09:42:33 -0700

    fix: support LargeStringArray for aggregated vals

--- spark/src/main/scala/org/apache/spark/sql/comet/operators.scala
+++ spark/src/main/scala/org/apache/spark/sql/comet/operators.scala
@@ -1984,7 +1984,7 @@ trait CometBaseAggregate {
     // If the aggregateExpressions is empty, we only want to build groupingExpressions,
     // and skip processing of aggregateExpressions.
     if (aggregateExpressions.isEmpty) {
-      val hashAggBuilder = OperatorOuterClass.HashAggregate.newBuilder()
+      val hashAggBuilder = newHashAggBuilder(aggregate)
       hashAggBuilder.addAllGroupingExprs(groupingExprs.map(_.get).asJava)
       // Spark has no expression mode to serialize here. An empty aggregate with a required child
       // distribution must fully deduplicate its keys (Final, or a pre-distinct PartialMerge), so
@@ -1995,8 +1995,6 @@ trait CometBaseAggregate {
         CometAggregateMode.Partial
       }
       hashAggBuilder.setModeValue(mode.getNumber)
-      hashAggBuilder.setUseLargeDataTypes(
-        CometConf.COMET_AGG_USE_LARGE_DATATYPES.get(aggregate.conf))
       buildAggOp(
         builder,
         hashAggBuilder,
@@ -2048,12 +2046,10 @@ trait CometBaseAggregate {
 
       if (childOp.nonEmpty && groupingExprs.forall(_.isDefined) &&
         aggExprs.forall(_.isDefined)) {
-        val hashAggBuilder = OperatorOuterClass.HashAggregate.newBuilder()
+        val hashAggBuilder = newHashAggBuilder(aggregate)
         hashAggBuilder.addAllGroupingExprs(groupingExprs.map(_.get).asJava)
         hashAggBuilder.addAllAggExprs(aggExprs.map(_.get).asJava)
         hashAggBuilder.setModeValue(mode.getNumber)
-        hashAggBuilder.setUseLargeDataTypes(
-          CometConf.COMET_AGG_USE_LARGE_DATATYPES.get(aggregate.conf))
 
         // Send per-expression modes and buffer offset for PartialMerge handling
         if (hasPartialMerge) {
@@ -2089,6 +2085,14 @@ trait CometBaseAggregate {
       }
     }
 
+  }
+
+  private def newHashAggBuilder(
+      aggregate: BaseAggregateExec): OperatorOuterClass.HashAggregate.Builder = {
+    val hashAggBuilder = OperatorOuterClass.HashAggregate.newBuilder()
+    hashAggBuilder.setUseLargeDataTypes(
+      CometConf.COMET_AGG_USE_LARGE_DATATYPES.get(aggregate.conf))
+    hashAggBuilder
   }
 
   /**
--- spark/src/test/scala/org/apache/comet/exec/CometAggregateSuite.scala
+++ spark/src/test/scala/org/apache/comet/exec/CometAggregateSuite.scala
@@ -37,11 +37,11 @@ import org.apache.spark.sql.execution.SQLExecution
 import org.apache.spark.sql.execution.adaptive.{AdaptiveSparkPlanExec, AdaptiveSparkPlanHelper, ShuffleQueryStageExec}
 import org.apache.spark.sql.execution.aggregate.{BaseAggregateExec, ObjectHashAggregateExec}
 import org.apache.spark.sql.execution.exchange.{ReusedExchangeExec, ShuffleExchangeExec}
-import org.apache.spark.sql.functions.{avg, col, count_distinct, expr, sum}
+import org.apache.spark.sql.functions.{avg, col, count_distinct, expr, grouping, sum}
 import org.apache.spark.sql.internal.SQLConf
 import org.apache.spark.sql.types.{ArrayType, DataTypes, StructField, StructType}
 
-import org.apache.comet.{CometConf, CometNativeException}
+import org.apache.comet.CometConf
 import org.apache.comet.CometConf.COMET_EXEC_STRICT_FLOATING_POINT
 import org.apache.comet.CometSparkSessionExtensions.{isSpark35Plus, isSpark40Plus, isSpark41Plus}
 import org.apache.comet.rules.CometExecRule
@@ -67,8 +67,6 @@ class CometAggregateSuite extends CometTestBase with AdaptiveSparkPlanHelper {
       .set(SQLConf.ANSI_ENABLED.key, "false")
       .set("spark.memory.offHeap.enabled", "false")
       .set(CometConf.COMET_ONHEAP_MEMORY_POOL_TYPE.key, "unbounded")
-      .set("spark.ui.enabled", "true")
-      .set("spark.ui.port", "4040")
 
   // TODO: To be addressed after DF 55 migration
   ignore("grouped aggregate metrics are forwarded without fabricating global metrics") {
@@ -3356,8 +3354,6 @@ class CometAggregateSuite extends CometTestBase with AdaptiveSparkPlanHelper {
   }
 
   test("CUBE(9) + COUNT(DISTINCT) wide Utf8 keys: useLargeDataTypes preserves correctness") {
-    import org.apache.spark.sql.functions.{count_distinct, grouping}
-
     def writeData(path: String, numRows: Long, wideBytes: Int): Unit = {
       val userPadLen = wideBytes - "user-".length
       val measurePadLen = wideBytes - "meas-".length
@@ -3428,20 +3424,19 @@ class CometAggregateSuite extends CometTestBase with AdaptiveSparkPlanHelper {
           val cometAggs = collectWithSubqueries(plan) { case a: CometHashAggregateExec => a }
           assert(
             cometAggs.nonEmpty,
-            s"With useLargeDataTypes=false, expected at least one " +
+            "With useLargeDataTypes=false, expected at least one " +
               s"CometHashAggregateExec in plan, got:\n$plan")
           val e = intercept[Throwable] {
             df.collect()
           }
-          val chain =
-            Iterator.iterate(e: Throwable)(_.getCause).takeWhile(_ != null).toList ++
-              Iterator
-                .iterate(e: Throwable)(_.getCause)
-                .takeWhile(_ != null)
-                .flatMap(c => Option(c.getSuppressed).toList.flatten)
+          val chain = Iterator
+            .iterate(e: Throwable)(_.getCause)
+            .takeWhile(_ != null)
+            .flatMap(c => c +: Option(c.getSuppressed).toList.flatten)
+            .toList
           assert(
             chain.exists(t => Option(t.getMessage).exists(_.contains("offset overflow"))),
-            s"With useLargeDataTypes=false, expected an 'offset overflow' error, got:\n" +
+            "With useLargeDataTypes=false, expected an 'offset overflow' error, got:\n" +
               chain.map(t => s"  ${t.getClass.getName}: ${t.getMessage}").mkString("\n"))
         }
       }
