d6a143690f3cc1f283a5640a93cc9d69ce4633d8
Kazantsev Maksim <mn.kazantsev@gmail.com>
2026-07-11 14:57:18 +0400

    refactoring

--- native/core/src/execution/lambda.rs
+++ native/core/src/execution/lambda.rs
@@ -15,11 +15,6 @@
 // specific language governing permissions and limitations
 // under the License.
 
-// Licensed to the Apache Software Foundation (ASF) under one or more
-// contributor license agreements.  See the NOTICE file distributed with
-// this work for additional information regarding copyright ownership.
-// ... (стандартный ASF-хедер)
-
 //! Helpers for planning DataFusion higher-order functions (HOFs) coming
 //! from Spark.
 //!
