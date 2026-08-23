891d101ab4f5f342dc31951162e0b636d9df907a
peterxcli <peterxcli@gmail.com>
2026-08-23 21:13:41 +0800

    perf: bulk copy fixed-width columns in ArrowWriter

--- spark/src/test/scala/org/apache/spark/sql/comet/execution/arrow/CometArrowStreamSuite.scala
+++ spark/src/test/scala/org/apache/spark/sql/comet/execution/arrow/CometArrowStreamSuite.scala
@@ -19,19 +19,21 @@
 
 package org.apache.spark.sql.comet.execution.arrow
 
+import java.nio.ByteOrder
+
 import scala.jdk.CollectionConverters._
 
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
 import org.apache.arrow.memory.{AllocationListener, RootAllocator}
-import org.apache.arrow.vector.{BaseValueVector, BigIntVector, BitVector, DecimalVector, IntervalMonthDayNanoVector, IntVector, VectorSchemaRoot}
+import org.apache.arrow.vector.{BaseFixedWidthVector, BaseValueVector, BigIntVector, BitVector, DecimalVector, IntervalMonthDayNanoVector, IntVector, VectorSchemaRoot}
 import org.apache.arrow.vector.types.pojo.{ArrowType, Field, FieldType, Schema}
-import org.apache.spark.sql.catalyst.expressions.GenericInternalRow
+import org.apache.spark.sql.catalyst.expressions.{GenericInternalRow, SpecializedGetters}
 import org.apache.spark.sql.comet.util.Utils
-import org.apache.spark.sql.execution.vectorized.OnHeapColumnVector
-import org.apache.spark.sql.types.{BooleanType, CalendarIntervalType, Decimal, DecimalType, IntegerType, StructField, StructType}
-import org.apache.spark.sql.vectorized.{ColumnarArray, ColumnarBatch}
+import org.apache.spark.sql.execution.vectorized.{Dictionary, OffHeapColumnVector, OnHeapColumnVector}
+import org.apache.spark.sql.types.{BooleanType, ByteType, CalendarIntervalType, DataType, DateType, DayTimeIntervalType, Decimal, DecimalType, DoubleType, FloatType, IntegerType, LongType, ShortType, StructField, StructType, TimestampNTZType, TimestampType, YearMonthIntervalType}
+import org.apache.spark.sql.vectorized.{ColumnarArray, ColumnarBatch, ColumnVector}
 import org.apache.spark.unsafe.types.CalendarInterval
 
 import org.apache.comet.vector.{CometPlainVector, CometVector}
@@ -156,6 +158,196 @@ class CometArrowStreamSuite extends AnyFunSuite with Matchers {
       requiredInput.close()
       booleanInput.close()
       decimalInput.close()
+      allocator.close()
+    }
+  }
+
+  test("bulk copy dispatch handles heap, off-heap, slices, and dictionary fallback") {
+    val allocator = new RootAllocator(Long.MaxValue)
+    val startRow = 3
+    val numRows = 32
+    val inputCapacity = startRow + numRows
+    val onHeap = new OnHeapColumnVector(inputCapacity, LongType)
+    val offHeap = new OffHeapColumnVector(inputCapacity, LongType)
+    val dictionary = new OnHeapColumnVector(inputCapacity, LongType)
+
+    class CountingLongWriter(vector: BigIntVector) extends LongWriter(vector) {
+      var scalarWrites: Int = 0
+
+      override protected def setValueUnsafe(input: SpecializedGetters, ordinal: Int): Unit = {
+        scalarWrites += 1
+        super.setValueUnsafe(input, ordinal)
+      }
+    }
+
+    def check(
+        input: ColumnVector,
+        rows: Int,
+        expected: Int => Long,
+        expectedScalarWrites: Int): Unit = {
+      val output = new BigIntVector("long", allocator)
+      output.allocateNew(rows)
+      val writer = new CountingLongWriter(output)
+      try {
+        writer.writeColumnSlice(input, startRow, rows)
+        writer.finish()
+
+        writer.scalarWrites shouldBe expectedScalarWrites
+        output.getValueCount shouldBe rows
+        var i = 0
+        while (i < rows) {
+          output.isNull(i) shouldBe false
+          output.get(i) shouldBe expected(i)
+          i += 1
+        }
+      } finally {
+        output.close()
+      }
+    }
+
+    try {
+      var i = 0
+      while (i < inputCapacity) {
+        onHeap.putLong(i, 1000L + i)
+        offHeap.putLong(i, 2000L + i)
+        i += 1
+      }
+
+      val scalarWrites =
+        if (ByteOrder.nativeOrder() == ByteOrder.LITTLE_ENDIAN) 0 else numRows
+      check(onHeap, numRows, i => 1000L + startRow + i, scalarWrites)
+      check(offHeap, numRows, i => 2000L + startRow + i, scalarWrites)
+      check(onHeap, 8, i => 1000L + startRow + i, 8)
+
+      dictionary.setDictionary(new Dictionary {
+        override def decodeToInt(id: Int): Int = id
+        override def decodeToLong(id: Int): Long = 3000L + id
+        override def decodeToFloat(id: Int): Float = id.toFloat
+        override def decodeToDouble(id: Int): Double = id.toDouble
+        override def decodeToBinary(id: Int): Array[Byte] = Array(id.toByte)
+      })
+      val dictionaryIds = dictionary.reserveDictionaryIds(inputCapacity)
+      i = 0
+      while (i < inputCapacity) {
+        dictionaryIds.putInt(i, i)
+        i += 1
+      }
+      check(dictionary, numRows, i => 3000L + startRow + i, numRows)
+    } finally {
+      onHeap.close()
+      offHeap.close()
+      dictionary.close()
+      allocator.close()
+    }
+  }
+
+  test("bulk copy dispatch covers supported fixed-width Arrow vectors") {
+    val allocator = new RootAllocator(Long.MaxValue)
+    val numRows = 32
+    class CountingFixedWidthWriter(override val valueVector: BaseFixedWidthVector)
+        extends FixedWidthArrowFieldWriter {
+      var scalarWrites: Int = 0
+      override def setValue(input: SpecializedGetters, ordinal: Int): Unit = scalarWrites += 1
+      override protected def setValueUnsafe(input: SpecializedGetters, ordinal: Int): Unit =
+        scalarWrites += 1
+    }
+    val dataTypes: Seq[DataType] = Seq(
+      ByteType,
+      ShortType,
+      IntegerType,
+      LongType,
+      FloatType,
+      DoubleType,
+      DateType,
+      TimestampType,
+      TimestampNTZType,
+      YearMonthIntervalType(),
+      DayTimeIntervalType())
+
+    try {
+      dataTypes.foreach { dataType =>
+        val input = new OnHeapColumnVector(numRows, dataType)
+        val output = Utils
+          .toArrowField("value", dataType, nullable = false, "UTC")
+          .createVector(allocator)
+          .asInstanceOf[BaseFixedWidthVector]
+        output.allocateNew(numRows)
+        val writer = new CountingFixedWidthWriter(output)
+
+        try {
+          writer.writeColumnSlice(input, 0, numRows)
+          writer.scalarWrites shouldBe
+            (if (ByteOrder.nativeOrder() == ByteOrder.LITTLE_ENDIAN) 0 else numRows)
+        } finally {
+          input.close()
+          output.close()
+        }
+      }
+    } finally {
+      allocator.close()
+    }
+  }
+
+  test("Arrow writer preserves row count for zero-column batches") {
+    val allocator = new RootAllocator(Long.MaxValue)
+    val root = VectorSchemaRoot.create(new Schema(Seq.empty[Field].asJava), allocator)
+    val numRows = 17
+    val input = new ColumnarBatch(Array.empty[ColumnVector], numRows)
+
+    try {
+      val writer = ArrowWriter.create(root, numRows)
+      writer.writeColumns(input, 0, numRows)
+      writer.finish()
+
+      root.getRowCount shouldBe numRows
+    } finally {
+      input.close()
+      root.close()
+      allocator.close()
+    }
+  }
+
+  test("Spark columnar reader preserves split input slice offsets") {
+    val allocator = new RootAllocator(Long.MaxValue)
+    val numRows = 12
+    val maxRecordsPerBatch = 5
+    val schema = StructType(Seq(StructField("long", LongType, nullable = false)))
+    val column = new OnHeapColumnVector(numRows, LongType)
+    val input = new ColumnarBatch(Array(column), numRows)
+    val reader = new SparkColumnarArrowReader(
+      allocator,
+      Utils.toArrowSchema(schema, "UTC"),
+      Iterator.single(input),
+      maxRecordsPerBatch)
+
+    try {
+      var i = 0
+      while (i < numRows) {
+        column.putLong(i, i * 7L)
+        i += 1
+      }
+
+      var startRow = 0
+      while (startRow < numRows) {
+        reader.loadNextBatch() shouldBe true
+        val outputRows = math.min(maxRecordsPerBatch, numRows - startRow)
+        val root = reader.getVectorSchemaRoot
+        root.getRowCount shouldBe outputRows
+
+        val longs = root.getVector(0).asInstanceOf[BigIntVector]
+        i = 0
+        while (i < outputRows) {
+          val sourceRow = startRow + i
+          longs.isNull(i) shouldBe false
+          longs.get(i) shouldBe sourceRow * 7L
+          i += 1
+        }
+        startRow += outputRows
+      }
+      reader.loadNextBatch() shouldBe false
+    } finally {
+      reader.close()
+      input.close()
       allocator.close()
     }
   }
