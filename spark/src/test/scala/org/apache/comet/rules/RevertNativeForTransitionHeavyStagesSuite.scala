b453c7482a0c9d0e8f0f0d7deda6dba0feecc47d
sam-1112 <sam22854338@gmail.com>
2026-09-16 10:52:18 +0800

    fix: remove unused import

--- spark/src/test/scala/org/apache/comet/rules/RevertNativeForTransitionHeavyStagesSuite.scala
+++ spark/src/test/scala/org/apache/comet/rules/RevertNativeForTransitionHeavyStagesSuite.scala
@@ -19,7 +19,6 @@
 
 package org.apache.comet.rules
 
-
 import org.apache.spark.sql.{CometTestBase, SaveMode}
 import org.apache.spark.sql.catalyst.expressions.{AttributeReference, Literal}
 import org.apache.spark.sql.catalyst.expressions.aggregate.{Final, Partial}
