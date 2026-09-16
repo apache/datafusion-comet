baa2281db5da2e0faa0d54758e2557d02cbb2c2a
Bhargava Vadlamani <bhargava@apache.org>
2026-09-16 15:40:44 -0500

    bench_agg_stat_functions

--- native/spark-expr/benches/nondeterministic.rs
+++ native/spark-expr/benches/nondeterministic.rs
@@ -29,7 +29,7 @@ use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
 use datafusion::physical_expr::expressions::Column;
 use datafusion::physical_expr::PhysicalExpr;
 use datafusion_comet_spark_expr::{
-    BernoulliCellSampler, MonotonicallyIncreasingId, RandExpr, RandStrExpr, RandnExpr, ShuffleExpr,
+    BernoulliCellSampler, monotonically_increasing_id::MonotonicallyIncreasingId, RandExpr, RandStrExpr, RandnExpr, ShuffleExpr,
     UuidExpr,
 };
 use std::hint::black_box;
--- native/spark-expr/src/lib.rs
+++ native/spark-expr/src/lib.rs
@@ -84,7 +84,7 @@ pub use datetime_funcs::{
 };
 pub use error::{decimal_overflow_error, SparkError, SparkErrorWithContext, SparkResult};
 pub use hash_funcs::*;
-pub use json_funcs::{FromJson, ToJson};
+pub use json_funcs::{FromJson, JsonArrayLength, ToJson};
 pub use math_funcs::{
     abs, checked_add, checked_div, checked_mul, checked_sub, create_modulo_expr,
     create_negate_expr, spark_ceil, spark_decimal_div, spark_decimal_integral_div, spark_floor,
